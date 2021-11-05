648a60e7ba311d7d5fc427485df875c244ebf24f
dione24 <dioneniangaly@gmail.com>
2021-11-05 09:56:40 +0000

    j++++

--- Library/Models/JournalManagerPDO.class.php
+++ Library/Models/JournalManagerPDO.class.php
@@ -453,10 +453,12 @@ class JournalManagerPDO extends JournalManager
 
     public function Reserve()
     {
+        $date = $_POST['daycloture'];
+
         $requeteAddService = $this->dao->prepare("INSERT INTO TbleCompte(RefAgency,SoldeCompte,DateSolde) VALUES(:RefAgency,:SoldeCompte,:DateSolde)");
         $requeteAddService->bindValue(':RefAgency', $_POST['RefAgency'], \PDO::PARAM_INT);
         $requeteAddService->bindValue(':SoldeCompte', $_POST['ReserveActuelle'], \PDO::PARAM_STR);
-        $requeteAddService->bindValue(':DateSolde', date($_POST['daycloture']), \PDO::PARAM_STR);
+        $requeteAddService->bindValue(':DateSolde', date("$date" . 'H:i:s'), \PDO::PARAM_STR);
         $requeteAddService->execute();
     }
     public function SomnmeVersementCaisse($Date, $Caisse)

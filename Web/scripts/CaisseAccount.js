4d362b802f87d0d8d1e3b5a177c92be9a7495feb
dione24 <dioneniangaly@gmail.com>
2021-11-12 00:09:19 +0000

    vvv

--- Web/scripts/CaisseAccount.js
+++ Web/scripts/CaisseAccount.js
@@ -1,9 +1,6 @@
 $(function () {
     var $RefCaisse = $('#RefCaisse');
     var $NumCompte = $('#NumCompte');
-   var $NameClient = $('#NameClient');
-    var Name;
-    
     $RefCaisse.on('click', function () {
         var val = $(this).val();
         if (val != null) $NumCompte.empty();
@@ -14,25 +11,10 @@ $(function () {
             success: function (json) {
                 if (json != null) {
                     $NumCompte.val(json['NUMCOMPTE']);
-                    Name = json['NUMCOMPTE'];
                 } else {
                     $NumCompte.val('');
                 }
             }
         });
-
-                 $.ajax({
-            url: '/config/client.php',
-            data: 'NumCompte=' + Name,
-            dataType: 'json',
-            success: function (json) {
-                if (json != null) {
-                    $NameClient.val(json);
-                } else {
-                    $NameClient.val('');
-                }
-            }
-        });
-  
     })
 });

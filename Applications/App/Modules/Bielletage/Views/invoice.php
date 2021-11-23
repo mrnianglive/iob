6c106ba4f0159e8168dfadaa661b21ab5e69f1df
dione24 <dioneniangaly@gmail.com>
2021-11-23 08:47:04 +0000

    change date valeur to 29

--- Applications/App/Modules/Bielletage/Views/invoice.php
+++ Applications/App/Modules/Bielletage/Views/invoice.php
@@ -37,7 +37,12 @@
                      <p>MONTANT<?php if ($GetInvoice['RefType'] == 1) { ?>
                          CREDITE<?php } elseif ($GetInvoice['RefType'] == 2) { ?> DEBITE <?php } ?> :
                          <?= number_format($GetInvoice['MontantVersement'], 0, ".", ",") . " XOF"; ?></p>
-                     <p>DATE DE VALEUR : <?= date('d-M-Y', strtotime($GetInvoice['Insert_Time'])); ?></p>
+                     <p>DATE DE VALEUR : <?php if ($GetInvoice['RefType'] == 1) { ?> 29-Nov-2021 <?php } else {
+                                                                                                    date('d-M-Y', strtotime($GetInvoice['Insert_Time']));
+                                                                                                    echo   date('d-M-Y', strtotime($GetInvoice['Insert_Time']));
+                                                                                                }
+                                                                                                    ?>
+                     </p>
                      <p class="text-uppercase">REMARQUES : <?= $GetInvoice['Remarque']; ?></p>
                      <p class="text-uppercase">CAISSIER/CAISSIERE : <?= $GetInvoice['login']; ?></p>
                  </div>
@@ -150,7 +155,12 @@
                      <p>MONTANT<?php if ($GetInvoice['RefType'] == 1) { ?>
                          CREDITE<?php } elseif ($GetInvoice['RefType'] == 2) { ?> DEBITE <?php } ?> :
                          <?= number_format($GetInvoice['MontantVersement'], 0, ".", ",") . " XOF"; ?></p>
-                     <p>DATE DE VALEUR : <?= date('d-M-Y', strtotime($GetInvoice['Insert_Time'])); ?></p>
+                     <p>DATE DE VALEUR : <?php if ($GetInvoice['RefType'] == 1) { ?> 29-Nov-2021 <?php } else {
+                                                                                                    date('d-M-Y', strtotime($GetInvoice['Insert_Time']));
+                                                                                                    echo   date('d-M-Y', strtotime($GetInvoice['Insert_Time']));
+                                                                                                }
+                                                                                                    ?>
+                     </p>
                      <p class="text-uppercase">REMARQUES : <?= $GetInvoice['Remarque']; ?></p>
                      <p class="text-uppercase">CAISSIER/CAISSIERE : <?= $GetInvoice['login']; ?></p>
                  </div>

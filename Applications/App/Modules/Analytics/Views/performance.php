d58fdc914ecb9739f2cbb7038153b09c4d3b3170
dione24 <dioneniangaly@gmail.com>
2021-11-23 23:08:13 +0000

    move

--- Applications/App/Modules/Analytics/Views/performance.php
+++ Applications/App/Modules/Analytics/Views/performance.php
@@ -62,19 +62,20 @@
 
 <div class="row">
     <div class="col-md-12">
-        <form method="POST">
-            <div class="input-group">
-                <div class="col-md-3">Date
-                    <input type="date" id="jour" name="jour" value="<? //= $day; 
-                                                                    ?>" class="form-control">
-                </div>
-                <div class="col-md-1"></br>
-                    <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
-                </div>
-            </div>
-        </form><br />
+
         <div class="white-box">
             <h3 class="box-title">Performance Journaliere</h3>
+            <form method="POST">
+                <div class="input-group">
+                    <div class="col-md-3">Date
+                        <input type="date" id="jour" name="jour" value="<? //= $day; 
+                                                                        ?>" class="form-control">
+                    </div>
+                    <div class="col-md-1"></br>
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                    </div>
+                </div>
+            </form><br />
             <div class="table-responsive">
                 <table id="dataTable1" class="display nowrap" cellspacing="0" width="100%">
                     <thead>

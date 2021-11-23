1864f0faa77e4d8b216867410e64ae4c1fd0e8c4
dione24 <dioneniangaly@gmail.com>
2021-11-23 16:46:57 +0000

    check

--- Library/Models/JournalManagerPDO.class.php
+++ Library/Models/JournalManagerPDO.class.php
@@ -460,8 +460,7 @@ class JournalManagerPDO extends JournalManager
         $requeteAddService->bindValue(':DateSolde', $_POST['daycloture'], \PDO::PARAM_STR);
         $requeteAddService->execute();
         $RefCompte = $this->dao->lastInsertId();
-        $requete = $this->dao->prepare("UPDATE TbleCompte SET DateSolde = CONCAT(DATE(DateSolde),' '$time'') WHERE RefCompte=:RefCompte");
-        $requete->bindValue(':RefCompte', $RefCompte, \PDO::PARAM_INT);
+        $requete = $this->dao->prepare("UPDATE TbleCompte SET DateSolde = CONCAT(DATE(DateSolde),' '$time'') WHERE RefCompte=$RefCompte");
         $requete->execute();
     }
     public function SomnmeVersementCaisse($Date, $Caisse)

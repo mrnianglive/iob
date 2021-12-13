049ab15695ddacc93f2a3266d1ec66b21574f8ee
dione24 <dioneniangaly@gmail.com>
2021-12-13 11:31:28 +0000

    laslog

--- Library/Models/UserManagerPDO.class.php
+++ Library/Models/UserManagerPDO.class.php
@@ -18,9 +18,9 @@ class UserManagerPDO extends UserManager
         $LogHour = $this->getLastConnexionTime($resultat['LastLogID']);
         //echo  $CurrentHour = date('Y-m-d H:i:s');
         $Pastmin = date('Y-m-d H:i:s', strtotime('-3 min'));
-        //  echo  $lastLog = date('H:i:s', strtotime($LogHour['LogH'] . "+3 min"));
+        echo  $lastLog = date('H:i:s', strtotime($LogHour['LogH'] . "+3 min"));
 
-        echo  date("H:i:s"); // this format is string comparable
+        // echo  date("H:i:s"); // this format is string comparable
         // if (($date_now > $lastLog) && ($date_now < date($LogHour['DateLog']))) {
         //  echo 'greater than';
         //  } else {

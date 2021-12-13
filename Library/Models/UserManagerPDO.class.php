6dae33c60cb15ec290742b29718c48396d5bdd4b
dione24 <dioneniangaly@gmail.com>
2021-12-13 23:01:47 +0000

    remove or

--- Library/Models/UserManagerPDO.class.php
+++ Library/Models/UserManagerPDO.class.php
@@ -22,7 +22,7 @@ class UserManagerPDO extends UserManager
         //echo date('H:i:s', $last);
         // echo gmdate("H:i:s");
         if (password_verify($_POST['password'], $resultat['password'])) {
-            if (((date('H:i:s', $last) > gmdate("H:i:s")) && date('Y-m-d') == date('Y-m-d', strtotime($LogHour['DateLog']))) or $resultat['log'] == 1) {
+            if ((date('H:i:s', $last) > gmdate("H:i:s")) && date('Y-m-d') == date('Y-m-d', strtotime($LogHour['DateLog']))) {
                 $_SESSION['message']['type'] = 'warning';
                 $_SESSION['message']['text'] = 'Utilisateur déjà connecté !';
                 $_SESSION['message']['number'] = 2;

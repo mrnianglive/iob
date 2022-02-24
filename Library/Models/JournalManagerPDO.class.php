1c89a120867b642f5bd74ccb5b5df13e82109d91
dione24 <dioneniangaly@gmail.com>
2022-02-24 12:29:57 +0000

    CancelValidate

--- Library/Models/JournalManagerPDO.class.php
+++ Library/Models/JournalManagerPDO.class.php
@@ -305,7 +305,7 @@ class JournalManagerPDO extends JournalManager
     }
     public function CancelValidate($id)
     {
-        $requete = $this->dao->prepare("UPDATE TbleOperations SET Validate= 1,DateValidate=NULL,RefValidate=NULL,ValidateDate=NULL WHERE RefOperations=:RefOperations");
+        $requete = $this->dao->prepare("UPDATE TbleOperations SET Validate= 1,DateValidate=NULL,RefValidate=NULL,ValidateDate=NULL,SentFromAgency=NULL WHERE RefOperations=:RefOperations");
         $requete->bindValue(':RefOperations', $id, \PDO::PARAM_STR);
         $requete->execute();
     }

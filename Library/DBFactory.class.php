ff750cdde8cf2d4043f1993f994d8b3cbceba0cf
dione24 <dioneniangaly@gmail.com>
2022-03-16 20:08:58 +0000

    git comit

--- Library/DBFactory.class.php
+++ Library/DBFactory.class.php
@@ -6,13 +6,13 @@ class DBFactory
 {
 	public static function MySQLPDO()
 	{
-		$db = new \PDO('mysql:host=localhost;dbname=iob;charset=utf8', 'root', 'root');
+		$db = new \PDO('mysql:host=localhost;dbname=iob;charset=utf8', 'root', '');
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
 		return $db;
 	}
 	public static function MySQLMySQLi()
 	{
-		return new \MySQLi('localhost', 'root', 'root', 'iob');
+		return new \MySQLi('localhost', 'root', '', 'iob');
 	}
 }

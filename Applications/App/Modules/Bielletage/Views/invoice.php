0fdd4ec4ffd96cb592fe5c5de2a1fb9437561848
dione24 <dioneniangaly@gmail.com>
2022-03-16 20:51:41 +0000

    aussi

--- Applications/App/Modules/Bielletage/Views/invoice.php
+++ Applications/App/Modules/Bielletage/Views/invoice.php
@@ -5,7 +5,7 @@
                  <div style="width: 10%;display: inline-block;vertical-align: top;">
                      <img src="/bordereau/mlc.jpg" alt="Logo" style="height: 40px;" width="100%">
                      <img src="/bordereau/ecobank.jpg" alt="Logo" style="height: 40px;" width="100%">
-                     <?php QRcode::png('PHP QR Code :)'); ?>
+                     <?php \QRcode::png('PHP QR Code :)'); ?>
                  </div>
                  <div style="text-align: center;width:88%;display: inline-block;">
                      <p>MALI CREANCES SA - PARTENAIRE ECOBANK MALI</p>

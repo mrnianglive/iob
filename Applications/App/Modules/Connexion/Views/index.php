b8521bcabe007403393a79ddde2d8da1b405dd27
dione24 <dioneniangaly@gmail.com>
2022-04-13 16:25:52 +0000

    logo

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -18,8 +18,8 @@
 
 
 <form method="post">
-    <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
-        height="72">
+    <img class="mb-4" src="/images/mlc.png" alt="mlc" width="72" height="72">
+    height="72">
     <h1 class="h3 mb-3 font-weight-normal">Connectez Vous</h1>
     <label for="inputEmail" class="sr-only">Login</label>
     <input type="email" id="login" class="form-control" placeholder="Login" required autofocus>

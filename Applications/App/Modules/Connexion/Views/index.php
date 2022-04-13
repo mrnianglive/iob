33e691ed7c50ad89a4d8e83204b194aae4460188
dione24 <dioneniangaly@gmail.com>
2022-04-13 16:33:36 +0000

    remove text

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -19,8 +19,6 @@
 
 <form method="post">
     <img class="mb-4" src="/images/mlc.png" alt="mlc" width="200" height="200">
-    <h1 class="h3 mb-3 font-weight-normal">Connectez Vous</h1>
-
     <label for="inputEmail" class="sr-only">Login</label>
     <input type="email" id="login" class="form-control" placeholder="Login" required autofocus>
     <span id="statut"></span>

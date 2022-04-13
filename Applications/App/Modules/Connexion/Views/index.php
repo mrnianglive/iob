a751580c934ccf61f105f300ae40bf6b954ac3a5
dione24 <dioneniangaly@gmail.com>
2022-04-13 16:37:02 +0000

    final

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -20,10 +20,10 @@
 <form method="post">
     <img src="/images/mlc.png" alt="mlc" width="200" height="100%">
     <label for="inputEmail" class="sr-only">Login</label>
-    <input type="text" id="login" class="form-control" placeholder="Login" required autofocus>
+    <input type="text" id="login" class="form-control" name="login" placeholder="Login" required autofocus>
     <span id="statut"></span>
     <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
     </br>
     <button class="btn btn-lg btn-primary btn-block" type="submit" id="register">Connexion</button>
     <p class="mt-5 mb-3 text-muted">&copy; MALI CREANCES 2021 Develop by NIANGALY</p>

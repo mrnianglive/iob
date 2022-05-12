c3eb160fb5fc5a18a6fdec1b8a9c75c8310ed5c7
dione24 <dioneniangaly@gmail.com>
2022-05-12 10:52:45 +0000

    ddd

--- Applications/App/Modules/Remittance/Views/index.php
+++ Applications/App/Modules/Remittance/Views/index.php
@@ -19,8 +19,9 @@
                 <div class="col-md-3">Au
                     <input type="date" id="Fin" name="Fin" value="<?= $Fin; ?>" class="form-control">
                 </div>
-                <br><br>
+
                 <div class="col-md-1">
+                    <br><br>
                     <button class="btn btn-primary" type="submit"><i class="fa fa-search"></i></button>
                 </div>
             </div>

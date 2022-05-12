7f642584b376acca8a6f67ef31ffaaa86993da26
dione24 <dioneniangaly@gmail.com>
2022-05-12 11:09:50 +0000

    changes col

--- Applications/App/Modules/Remittance/Views/index.php
+++ Applications/App/Modules/Remittance/Views/index.php
@@ -30,7 +30,7 @@
 
 
         <form class="form-inline" method="POST">
-            <div class="form-group col-md-3">
+            <div class="form-group col-md-4">
                 <label for="staticEmail2" class="sr-only">Agence</label>
                 <select class="form-control" name="RefAgency" tabindex="1" required="">
                     <?php foreach ($UserAgence as $key => $Agence) {
@@ -41,16 +41,16 @@
                     <?php }   ?>
                 </select>
             </div>
-            <div class="form-group col-md-3">
+            <div class="form-group col-md-4">
                 <label for="inputPassword2" class="sr-only">Du</label>
                 <input type="date" id="Debut" name="Debut" value="<?= $Debut; ?>" class="form-control ">
             </div>
 
-            <div class="form-group col-md-3">
+            <div class="form-group col-md-4">
                 <label for="inputPassword2" class="sr-only">Au</label>
                 <input type="date" id="Fin" name="Fin" value="<?= $Fin; ?>" class="form-control">
             </div>
-            <button type="submit" class="btn btn-primary col-md-1"><i class="fa fa-search"></i></button>
+            <button type="submit" class="btn btn-primary "><i class="fa fa-search"></i></button>
         </form><br />
 
 

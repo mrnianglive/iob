01383b9bc46c75b0efc9a8f49b49a5265169df5c
dione24 <dioneniangaly@gmail.com>
2022-05-12 10:59:57 +0000

    minus search

--- Applications/App/Modules/Remittance/Views/index.php
+++ Applications/App/Modules/Remittance/Views/index.php
@@ -19,10 +19,9 @@
                 <div class="col-md-3">Au
                     <input type="date" id="Fin" name="Fin" value="<?= $Fin; ?>" class="form-control">
                 </div>
-                <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                <button type="submit" class="btn btn-primary"><i class="fa fa-search-minus"></i></button>
 
             </div>
-
         </form><br />
         <div class="white-box">
             <h3 class="box-title">Opérations</h3>

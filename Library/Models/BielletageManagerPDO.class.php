73347c2e117ffc2cf4119c7ff2ad5688a221ef99
dione24 <dioneniangaly@gmail.com>
2022-07-18 12:54:04 +0000

    add mew template

--- Library/Models/BielletageManagerPDO.class.php
+++ Library/Models/BielletageManagerPDO.class.php
@@ -316,17 +316,26 @@ class BielletageManagerPDO extends BielletageManager
     }
 
 
-
     public function AlerteSortie($caisse, $montant)
     {
         $query = $this->dao->prepare('SELECT * FROM TbleCaisse INNER JOIN TbleAgency ON TbleAgency.RefAgency=TbleCaisse.RefAgency WHERE TbleCaisse.RefCaisse=:RefCaisse');
         $query->bindValue(':RefCaisse', $caisse, \PDO::PARAM_INT);
         $query->execute();
         $result = $query->fetch();
-        $subject = "ALERTE SORTIE DE FONDS | CAISSE MLC";
-        $message = "Alerte de sortie de fonds de la caisse  : " . $result['NameCaisse'] . " " . $result['NameAgency'] . " d'un  montant de : " . $montant . " FCFA";
-        $headers = 'From: no-reply@app.malicreances-sa.com' . "\r\n" .
+
+        $from = "no-reply@malicreances-sa.com";
+        require_once __DIR__ . '/../../Applications/App/Templates/templatemail.php';
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $to = "control@malicreances-sa.com";
+        // Create email headers
+        $headers .= 'From: ' . $from . "\r\n" .
+            'Reply-To: ' . $from . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
-        mail("control@malicreances-sa.com", $subject, $message, $headers);
+
+        $subject = "ALERTE SORTIE DE FONDS | CAISSE MLC";
+        $atitle = $subject;
+        $alert = "Alerte de sortie de fonds de la caisse  : " . $result['NameCaisse'] . " " . $result['NameAgency'] . " d'un  montant de : " . $montant . " FCFA";
+        mail($to, $subject, $content, $headers);
     }
 }

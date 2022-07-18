d0a1b503c6f34942a628fb12430a49ec7e193776
dione24 <dioneniangaly@gmail.com>
2022-07-18 13:03:14 +0000

    change email

--- Library/Models/BielletageManagerPDO.class.php
+++ Library/Models/BielletageManagerPDO.class.php
@@ -327,7 +327,7 @@ class BielletageManagerPDO extends BielletageManager
         require_once __DIR__ . '/../../Applications/App/Templates/templatemail.php';
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        $to = "control@malicreances-sa.com";
+        $to = "control_iob@malicreances-sa.com";
         // Create email headers
         $headers .= 'From: ' . $from . "\r\n" .
             'Reply-To: ' . $from . "\r\n" .

a4c8c5e49c068420d4b8211c9732e0cabb343ecd
dione24 <dioneniangaly@gmail.com>
2022-07-18 13:16:24 +0000

    correction

--- Library/Models/BielletageManagerPDO.class.php
+++ Library/Models/BielletageManagerPDO.class.php
@@ -324,10 +324,10 @@ class BielletageManagerPDO extends BielletageManager
         $result = $query->fetch();
 
         $from = "no-reply@malicreances-sa.com";
-        $subject = "ALERTE SORTIE DE FONDS | CAISSE MLC";
+        $subject = "SORTIE DE FONDS ";
 
         $atitle = $subject;
-        $alert = "Alerte de sortie de fonds de la caisse  : " . $result['NameCaisse'] . " " . $result['NameAgency'] . " d'un  montant de : " . $montant . " FCFA";
+        $alert = "Sortie de fonds de la caisse  : " . $result['NameCaisse'] . " " . $result['NameAgency'] . " d'un  montant de : " . $montant . " FCFA";
 
         require_once __DIR__ . '/../../Applications/App/Templates/templatemail.php';
         $headers  = 'MIME-Version: 1.0' . "\r\n";

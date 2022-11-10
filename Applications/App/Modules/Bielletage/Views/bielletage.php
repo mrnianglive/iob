84b357c959c5a7ba02591905b27d92b434d67fc5
dione24 <dioneniangaly@gmail.com>
2022-11-10 17:09:17 +0000

    adding new interfaces

--- Applications/App/Modules/Bielletage/Views/bielletage.php
+++ Applications/App/Modules/Bielletage/Views/bielletage.php
@@ -3,18 +3,21 @@
         <li class="nav-item">
             <a class="nav-link" href="#step-1">
                 Bielletage-<?php if ($_GET['id'] == 1) { ?>Versement<?php } elseif ($_GET['id'] == 2) { ?>Retrait<?php } elseif ($_GET['id'] == 3) { ?>Appro
-                Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de Fond<?php } ?>
+                Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de
+                Fond<?php } elseif ($_GET['id'] == 5) { ?>Transfert Caisse2Caisse<?php } ?>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#step-2">
                 Bielletage-<?php if ($_GET['id'] == 1) { ?>Versement<?php } elseif ($_GET['id'] == 2) { ?>Retrait<?php } elseif ($_GET['id'] == 3) { ?>Appro
-                Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de Fond<?php } ?> </a>
+                Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de
+                Fond<?php } elseif ($_GET['id'] == 5) { ?>Transfert Caisse2Caisse<?php } ?> </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#step-3">
                 Bielletage-<?php if ($_GET['id'] == 1) { ?>Versement<?php } elseif ($_GET['id'] == 2) { ?>Retrait<?php } elseif ($_GET['id'] == 3) { ?>Appro
-                Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de Fond<?php } ?> </a>
+                Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de
+                Fond<?php } elseif ($_GET['id'] == 5) { ?>Transfert Caisse2Caisse<?php } ?> </a>
         </li>
     </ul>
     <form method="POST" action='/bielletage/add'>
@@ -274,6 +277,9 @@
                     </div>
                 </div>
             </div>
+
+            <?php if ($_GET['id'] != 5) { ?>
+
             <div id="step-3" class="tab-pane" role="tabpanel" aria-labelledby="step-3">
                 <div class="row">
                     <?php if ($_GET['id'] == 3 or $_GET['id'] == 4) { ?>
@@ -286,12 +292,12 @@
                                 <label class="control-label">Caisse</label>
                                 <select class="form-control" name="RefCaisse" tabindex="1" required="">
                                     <?php foreach ($CheckOuverture as $key => $Caisse) {
-                                            if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
-                                        ?>
+                                                if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
+                                            ?>
                                     <option value="<?= $Caisse['RefCaisse']; ?>">
                                         <?= $Caisse['NameCaisse'] . " " . $Caisse['NameAgency']; ?></option>
                                     <?php }
-                                        }   ?>
+                                            }   ?>
                                 </select>
                             </div>
                         </div>
@@ -303,20 +309,20 @@
 
                                     <?php
 
-                                                if ($_SESSION['statut'] == 'admin' or $_SESSION['statut'] == 'Control' or $_SESSION['statut'] == 'Head') {
-                                                    foreach ($TypeAppro as $key => $type) { ?>
+                                                    if ($_SESSION['statut'] == 'admin' or $_SESSION['statut'] == 'Control' or $_SESSION['statut'] == 'Head') {
+                                                        foreach ($TypeAppro as $key => $type) { ?>
                                     <option value="<?= $type['RefTypeAppro']; ?>">
                                         <?= $type['NameTypeAppro']; ?></option>
                                     <?php }
-                                                } else {
-                                                    foreach ($TypeAppro as $key => $type) {
-                                                        if ($type['RefTypeAppro'] == 1) { ?>
+                                                    } else {
+                                                        foreach ($TypeAppro as $key => $type) {
+                                                            if ($type['RefTypeAppro'] == 1) { ?>
                                     <option value="<?= $type['RefTypeAppro']; ?>">
                                         <?= $type['NameTypeAppro']; ?></option>
 
                                     <?php }
-                                                    }
-                                                } ?>
+                                                        }
+                                                    } ?>
 
                                 </select>
                             </div>
@@ -336,12 +342,12 @@
                                 <select class="form-control" name="RefCaisse" id="RefCaisse" tabindex="1" required="">
 
                                     <?php foreach ($CheckOuverture as $key => $Caisse) {
-                                                if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
-                                            ?>
+                                                    if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
+                                                ?>
                                     <option value="<?= $Caisse['RefCaisse']; ?>">
                                         <?= $Caisse['NameCaisse'] . " " . $Caisse['NameAgency']; ?></option>
                                     <?php }
-                                            }   ?>
+                                                }   ?>
                                 </select>
                             </div>
                         </div>
@@ -479,5 +485,86 @@
                 </div>
 
             </div>
-    </form>
+            <?php } else { ?>
+            <!---Debut Transfer-->
+            <div id="step-3" class="tab-pane" role="tabpanel" aria-labelledby="step-3">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label class="control-label">Source</label>
+                            <select class="form-control" name="RefCaisse" tabindex="1" required="">
+                                <?php foreach ($CheckOuverture as $key => $Caisse) {
+                                        if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
+                                    ?>
+                                <option value="<?= $Caisse['RefCaisse']; ?>">
+                                    <?= $Caisse['NameCaisse'] . " " . $Caisse['NameAgency']; ?></option>
+                                <?php }
+                                    }   ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label class="control-label">Destination</label>
+                            <select class="form-control" name="Destination" tabindex="1" required="">
+
+                                <?php foreach ($CheckOuverture as $key => $Caisse) {
+                                        if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
+                                    ?>
+                                <option value="<?= $Caisse['RefCaisse']; ?>">
+                                    <?= $Caisse['NameCaisse'] . " " . $Caisse['NameAgency']; ?></option>
+                                <?php }
+                                    }   ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class=" row">
+                    <div class="col-md-6">
+                        <div class="form-group has-error">
+                            <label class="control-label">Montant</label>
+                            <input type="int" id="total" class="form-control" name="MontantVersement" readonly>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="form-group has-error">
+                            <label class="control-label">Remarque</label>
+                            <input type="text" class="form-control" name="Remarque" value="Transfert Caisse2Caisse"
+                                readonly="" autocomplete="OFF">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group has-error">
+                            <label class="control-label">Deposant/Auteur Retrait</label>
+                            <input type="text" class="form-control" name="NameDeposant"
+                                value="<?= $_SESSION['PrenomUsers'], " " . $_SESSION['NomUsers']; ?>" readonly=""
+                                autocomplete="OFF">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group has-error">
+                            <label class="control-label">Téléphone</label>
+                            <input type="text" class="form-control" name="TelDeposant" value="NULL" readonly=""
+                                autocomplete="OFF">
+                        </div>
+                    </div>
+
+                    <?php if (in_array(3, $permission)) { ?>
+                    <div class="col-md-6">
+                        <div class="form-group has-error">
+                            <label class="control-label">Date</label>
+                            <input type="date" class="form-control" name="Antidate">
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+</div>
+
+
+
+<?php } ?>
+</form>
 </div>

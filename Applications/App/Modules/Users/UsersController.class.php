1041e696419be4f7c6c0f3cc78a130d1e375db53
dione24 <dioneniangaly@gmail.com>
2023-01-04 17:05:10 +0000

    fixed

--- Applications/App/Modules/Users/UsersController.class.php
+++ Applications/App/Modules/Users/UsersController.class.php
@@ -115,6 +115,6 @@ class UsersController extends \Library\BackController
         $this->page->addVar("titles", "Double authentification"); // Titre de la page
         $Info = $this->managers->getManagerOf('User')->GetUserInfo($request->getData('id'));
         $this->managers->getManagerOf("User")->ResetAuth($Info['RefUsers']);
-        $this->app()->httpResponse()->redirect('/Users/doubleauth/ ' . $Info['RefUsers']); //Retour en arriere   
+        $this->app()->httpResponse()->redirect('/Users/doubleauth/' . $Info['RefUsers']); //Retour en arriere   
     }
 }

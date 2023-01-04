4c00808c2a1b7f5ca56457c65b9d249e146beefa
dione24 <dioneniangaly@gmail.com>
2023-01-04 16:16:37 +0000

    success

--- Applications/App/Modules/Connexion/Views/doubleauth.php
+++ Applications/App/Modules/Connexion/Views/doubleauth.php
@@ -3,6 +3,6 @@
     <label for="inputEmail" class="sr-only">Code</label>
     <input type="text" id="tfa_code" class="form-control mb-1" name="tfa_code"
         placeholder="Veuillez saisir le code généré par l'authentificateur" required autofocus>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Valider</button>
+    <button class="btn btn-lg btn-success btn-block" type="submit">Valider</button>
     <p class="mt-5 mb-3 text-muted">&copy; MALI CREANCES 2021 Developed by NIANGALY</p>
 </form>

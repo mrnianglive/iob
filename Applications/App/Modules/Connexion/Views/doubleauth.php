ba317a3eaee57856d6db6f8565ab69d888b37701
dione24 <dioneniangaly@gmail.com>
2023-01-04 16:19:58 +0000

    VF AUTH

--- Applications/App/Modules/Connexion/Views/doubleauth.php
+++ Applications/App/Modules/Connexion/Views/doubleauth.php
@@ -4,5 +4,7 @@
     <input type="text" id="tfa_code" class="form-control mb-1" name="tfa_code"
         placeholder="Veuillez saisir le code généré par l'authentificateur" required autofocus>
     <button class="btn btn-lg btn-success btn-block" type="submit">Valider</button>
+    <br>
+    <a href="/Logout" class="btn btn-lg btn-warning btn-block">Annuler</a>
     <p class="mt-5 mb-3 text-muted">&copy; MALI CREANCES 2021 Developed by NIANGALY</p>
 </form>

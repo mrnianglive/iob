d11db030c48d33b97f9a79a39e811560fca09a0d
dione24 <dioneniangaly@gmail.com>
2023-02-06 12:53:18 +0000

    Okay

--- Web/script.php
+++ Web/script.php
@@ -14,22 +14,13 @@ if ($conn->connect_error) {
 $query = "SHOW TABLES";
 $result = $conn->query($query);
 
+$table_names = array();
 // Loop through each table
 while ($table = $result->fetch_array()) {
     $table_name = $table[0];
-    echo "Updating auto increment and defining primary key for table $table_name\n";
-
-    // Query to get the name of the first column in the table
-    $column_query = "DESCRIBE $table_name";
-    $column_result = $conn->query($column_query);
-    $column = $column_result->fetch_array();
-    $first_column_name = $column[0];
-    //fist colunn last id
-
-
-    echo json_encode(
-        $table_name
-    );
 }
+echo json_encode(
+    $table_name
+);
 
 $conn->close();

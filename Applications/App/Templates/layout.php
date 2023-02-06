5c326ccc2b543435de4eb1ceaecb2e0e01c5a2fc
dione24 <dioneniangaly@gmail.com>
2023-02-06 14:34:27 +0000

    esdgf

--- Applications/App/Templates/layout.php
+++ Applications/App/Templates/layout.php
@@ -92,12 +92,7 @@ if (!isset($_SESSION['DoubleAuth']) && isset($_SESSION['secret'])) {
                     <!-- ============================================================== -->
                     <ul class="navbar-nav ml-auto d-flex align-items-center">
                         <li>
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>Pays</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
+                            <button class="btn btn-info"><span>Pays</span></button>
                         </li>
 
                         <!-- ============================================================== -->

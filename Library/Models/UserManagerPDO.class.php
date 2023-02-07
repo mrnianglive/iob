1015cd9484f928026c52deffbcb23fcf3e03f5c5
dione24 <dioneniangaly@gmail.com>
2023-02-07 08:55:39 +0000

    Test

--- Library/Models/UserManagerPDO.class.php
+++ Library/Models/UserManagerPDO.class.php
@@ -87,18 +87,34 @@ class UserManagerPDO extends UserManager
     public function ListeUsers()
     {
         if ($_SESSION['statut'] == 'superadmin') {
-            $requeteUsers = $this->dao->prepare('SELECT * FROM TbleUsers INNER JOIN TbleStatut ON TbleStatut.RefStatut=TbleUsers.RefStatut LEFT JOIN tblpays ON tblpays.RefPays=TbleUsers.RefPays');
+            $sql = 'SELECT * FROM TbleUsers INNER JOIN TbleStatut ON TbleStatut.RefStatut=TbleUsers.RefStatut LEFT JOIN tblpays ON tblpays.RefPays=TbleUsers.RefPays';
         } else {
-            $requeteUsers = $this->dao->prepare('SELECT * FROM TbleUsers INNER JOIN TbleStatut ON TbleStatut.RefStatut=TbleUsers.RefStatut INNER JOIN tblpays ON tblpays.RefPays=TbleUsers.RefPays WHERE TbleUsers.RefPays=:RefPays');
+            $sql = 'SELECT * FROM TbleUsers INNER JOIN TbleStatut ON TbleStatut.RefStatut=TbleUsers.RefStatut INNER JOIN tblpays ON tblpays.RefPays=TbleUsers.RefPays WHERE TbleUsers.RefPays=:RefPays';
+        }
+
+        $requeteUsers = $this->dao->prepare($sql);
+
+        if ($_SESSION['statut'] != 'superadmin') {
             $requeteUsers->bindValue(':RefPays', $_SESSION['RefPays'], \PDO::PARAM_INT);
         }
+
         $requeteUsers->execute();
         $ListeUsers = $requeteUsers->fetchAll();
-        foreach ($ListeUsers as $key => $value) {
-            $ListeUsers[$key]['Verify'] = $this->VerifCaisse(NULL, $value['RefUsers']);
-        }
+
+        $ListeUsers = $this->addVerificationToUsers($ListeUsers);
+
         return $ListeUsers;
     }
+
+    private function addVerificationToUsers(array $users)
+    {
+        foreach ($users as $key => $value) {
+            $users[$key]['Verify'] = $this->VerifCaisse(NULL, $value['RefUsers']);
+        }
+
+        return $users;
+    }
+
 
     public function ListeCaisse()
     {

c190404fbbe2a6eb82e635422e6bdf684e23454f
dione24 <dioneniangaly@gmail.com>
2023-02-07 09:32:59 +0000

    ddd

--- Library/Models/PannelManagerPDO.class.php
+++ Library/Models/PannelManagerPDO.class.php
@@ -217,7 +217,7 @@ class PannelManagerPDO extends PannelManager
         if ($_SESSION['statut'] == 'superamdin') {
             $requete = $this->dao->prepare('SELECT (tbllinks.RefLinks,tbllinks.url,tbllinks.url_name,tbllinks.btn,tbllinks.target,tbllinks.RefPays,tblpays.nomPays) FROM tbllinks INNER JOIN tblpays ON tblpays.RefPays=tbllinks.RefPays');
         } else {
-            $requete = $this->dao->prepare('SELECT (tbllinks.RefLinks,tbllinks.url,tbllinks.url_name,tbllinks.btn,tbllinks.target,tbllinks.RefPays,tblpays.nomPays)FROM tbllinks INNER JOIN tblpays ON tblpays.RefPays=tbllinks.RefPays WHERE RefPays=:RefPays');
+            $requete = $this->dao->prepare('SELECT (tbllinks.RefLinks,tbllinks.url,tbllinks.url_name,tbllinks.btn,tbllinks.target,tbllinks.RefPays,tblpays.nomPays)FROM tbllinks INNER JOIN tblpays ON tblpays.RefPays=tbllinks.RefPays WHERE tbllinks.RefPays=:RefPays');
             $requete->bindValue(':RefPays', $_SESSION['RefPays'], \PDO::PARAM_INT);
         }
         $requete->execute();

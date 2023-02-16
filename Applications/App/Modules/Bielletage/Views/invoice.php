a6a0c3eab50f43315c5b045aa425c03546470041
dione24 <dioneniangaly@gmail.com>
2023-02-16 12:34:55 +0000

    ccc

--- Applications/App/Modules/Bielletage/Views/invoice.php
+++ Applications/App/Modules/Bielletage/Views/invoice.php
@@ -9,7 +9,7 @@
                  </div>
                  <?php } else { ?>
                  <div style="width: 10%;display: inline-block;vertical-align: top;">
-                     <img src="/images/afc.png" alt="Logo" style="height: 40px;" width="100%">
+                     <img src="/images/jpeg.jpeg" alt="Logo" style="height: 40px;" width="100%">
                  </div>
                  <?php } ?>
                  <div style="text-align: center;width:88%;display: inline-block;">
@@ -137,7 +137,7 @@
                  </div>
                  <?php } else { ?>
                  <div style="width: 10%;display: inline-block;vertical-align: top;">
-                     <img src="/images/afc.png" alt="Logo" style="height: 40px;" width="100%">
+                     <img src="/images/afch.jpeg" alt="Logo" style="height: 40px;" width="100%">
                  </div>
                  <?php } ?>
                  <div style="text-align: center;width:88%;display: inline-block;">

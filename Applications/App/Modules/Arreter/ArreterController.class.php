3dc5be610cbc89f88a25b908d660c42eab8bb210
dione24 <dioneniangaly@gmail.com>
2023-02-20 11:04:19 +0000

    Fixed again

--- Applications/App/Modules/Arreter/ArreterController.class.php
+++ Applications/App/Modules/Arreter/ArreterController.class.php
@@ -36,8 +36,8 @@ class ArreterController extends \Library\BackController
     {
         $this->managers->getManagerOf("Journal")->Reserve($request); //Arreter Reserve
         $Agence  = $this->managers->getManagerOf("Pannel")->GetAgency($request->postData('RefAgency')); //Recuperation de la liste
-        $Agence = $this->managers->getManagerOf('Journal')->CaisseAgence($Agence['RefAgency'], $request->postData('daycloture'));
-        foreach ($Agence as $clef => $data) {
+        $Caisse = $this->managers->getManagerOf('Journal')->CaisseAgence($Agence['RefAgency'], $request->postData('daycloture'));
+        foreach ($Caisse as $clef => $data) {
 
             $CheckClose = $this->managers->getManagerOf('Bielletage')->CheckDailyClose($data['RefCaisse']);
             if (empty($CheckClose)) {

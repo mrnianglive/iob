20c09d7a8bbf2af46caff561c1f95ca576b08419
dione24 <dioneniangaly@gmail.com>
2023-02-21 11:33:59 +0000

    Back

--- Applications/App/Modules/Bielletage/Views/index.php
+++ Applications/App/Modules/Bielletage/Views/index.php
@@ -27,7 +27,7 @@
           </div>
       </div>
   </form>
-  </br>
+
   <?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier') { ?>
       <div class="col-lg-12 col-sm-12 col-xs-12">
           <?php foreach ($Agence as $key => $value) {

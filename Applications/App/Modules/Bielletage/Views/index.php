c88a550b060e868e641071db25eb16d02108121a
dione24 <dioneniangaly@gmail.com>
2023-02-21 12:18:26 +0000

    Fixed

--- Applications/App/Modules/Bielletage/Views/index.php
+++ Applications/App/Modules/Bielletage/Views/index.php
@@ -1,156 +1,146 @@
- <form method="POST" id="formulaire">
-     <div class="input-group">
-         <div class="">
-             <select class="form-control" name="RefPays" tabindex="1" id="RefPays">
-                 <option value="0">Pays</option>
-                 <?php foreach ($Pays as $key => $Pays) { ?>
-                 <option value="<?= $Pays['RefPays']; ?>"
-                     <?= (isset($_POST['RefPays']) and $_POST['RefPays'] == $Pays['RefPays']) ? 'selected' : '' ?>>
-                     <?= $Pays['nomPays']; ?>
-                 </option>
-                 <?php } ?>
-             </select>
-         </div>
-         &nbsp;
-         <div class="">
-             <select class="form-control" name="RefAgency" tabindex="1" id="RefAgency">
-                 <option value="">Agence</option>
-             </select>
-         </div>
-         &nbsp;
-         <div class="">
-             <select class="form-control" name="RefCaisse" tabindex="1" id="RefCaisse">
-                 <option value="">Caisse</option>
-             </select>
-         </div>
-         &nbsp;
-         <div class="">
-             <button type="submit" class="btn btn-primary" data-toggle="tooltip"
-                 title="Cliquez ici pour lancer la recherche">
-                 <i class="fas fa-search"></i>
-             </button>
-         </div>
-     </div>
- </form>
+<form method="POST" id="formulaire">
+    <div class="input-group">
+        <div class="">
+            <select class="form-control" name="RefPays" tabindex="1" id="RefPays">
+                <option value="0">Pays</option>
+                <?php foreach ($Pays as $key => $Pays) { ?>
+                    <option value="<?= $Pays['RefPays']; ?>" <?= (isset($_POST['RefPays']) and $_POST['RefPays'] == $Pays['RefPays']) ? 'selected' : '' ?>>
+                        <?= $Pays['nomPays']; ?>
+                    </option>
+                <?php } ?>
+            </select>
+        </div>
+        &nbsp;
+        <div class="">
+            <select class="form-control" name="RefAgency" tabindex="1" id="RefAgency">
+                <option value="" data-desired-agency="desired_agency_value">Agence</option>
+            </select>
+        </div>
+        &nbsp;
+        <div class="">
+            <select class="form-control" name="RefCaisse" tabindex="1" id="RefCaisse">
+                <option value="" data-desired-caisse="desired_caisse_value">Caisse</option>
+            </select>
+        </div>
+        &nbsp;
+        <div class="">
+            <button type="submit" class="btn btn-primary" data-toggle="tooltip" title="Cliquez ici pour lancer la recherche">
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
+    </div>
+</form>
 
- &nbsp;
+&nbsp;
 
- <?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier') { ?>
- <div class="col-lg-12 col-sm-12 col-xs-12">
-     <?php foreach ($Agence as $key => $value) {
-                if ($value['SommeDepot'] ==  0) { ?>
-     <div class="alert alert-danger" role="alert">
-         <span class="badge badge-danger"><?= $value['NameAgency']; ?> | Appro</span> Le solde
-         de la reserve est : <?= number_format($value['YesterdayReserve'], 0, '.', '.'); ?> | Merci d'Approvisonner
-         l'Agence
-         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-             <span aria-hidden="true">&times;</span>
-         </button>
-     </div>
-     <?php  }
-            } ?>
- </div>
- <?php } ?>
- <div class="row justify-content-center">
-     <div class="col-lg-3 col-sm-6 col-xs-12">
-         <div class="white-box analytics-info">
-             <h3 class="box-title">DEPOT</h3>
-             <ul class="list-inline two-part d-flex align-items-center mb-0">
-                 <li class="ml-auto"><span
-                         class="counter text-danger"><?= number_format($SommeVersementGlobal, 0, '.', '.'); ?></span>
-                 </li>
-             </ul>
-             <span>CAISSE</span>
-         </div>
-     </div>
-     <div class="col-lg-3 col-sm-6 col-xs-12">
-         <div class="white-box analytics-info">
-             <h3 class="box-title">RETRAIT</h3>
-             <ul class="list-inline two-part d-flex align-items-center mb-0">
-                 <li class="ml-auto"><span
-                         class="counter text-purple"><?= number_format($SommeRetraitGlobal, 0, '.', '.'); ?></span>
-                 </li>
-             </ul>
-             <span>CAISSE</span>
-         </div>
-     </div>
-     <div class="col-lg-3 col-sm-6 col-xs-12">
-         <div class="white-box analytics-info">
-             <h5 class="box-title">SOLDE ESPECES</h5>
-             <ul class="list-inline two-part d-flex align-items-center mb-0">
-                 <li class="ml-auto"><span class="counter text-info">
-                         <?= number_format($SoldeGlobal, 0, '.', '.'); ?>
-                     </span>
-                 </li>
-             </ul>
-             <span>CAISSE</span>
-         </div>
-     </div>
-     <div class="col-lg-3 col-sm-6 col-xs-12">
-         <div class="white-box analytics-info">
-             <iframe
-                 src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Africa%2FBamako"
-                 width="100%" height="90" frameborder="0" seamless></iframe>
-         </div>
-     </div>
- </div>
- <?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier' or $_SESSION['statut'] == 'admin') { ?>
+<?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier') { ?>
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+        <?php foreach ($Agence as $key => $value) {
+            if ($value['SommeDepot'] ==  0) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <span class="badge badge-danger"><?= $value['NameAgency']; ?> | Appro</span> Le solde
+                    de la reserve est : <?= number_format($value['YesterdayReserve'], 0, '.', '.'); ?> | Merci d'Approvisonner
+                    l'Agence
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+        <?php  }
+        } ?>
+    </div>
+<?php } ?>
+<div class="row justify-content-center">
+    <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="white-box analytics-info">
+            <h3 class="box-title">DEPOT</h3>
+            <ul class="list-inline two-part d-flex align-items-center mb-0">
+                <li class="ml-auto"><span class="counter text-danger"><?= number_format($SommeVersementGlobal, 0, '.', '.'); ?></span>
+                </li>
+            </ul>
+            <span>CAISSE</span>
+        </div>
+    </div>
+    <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="white-box analytics-info">
+            <h3 class="box-title">RETRAIT</h3>
+            <ul class="list-inline two-part d-flex align-items-center mb-0">
+                <li class="ml-auto"><span class="counter text-purple"><?= number_format($SommeRetraitGlobal, 0, '.', '.'); ?></span>
+                </li>
+            </ul>
+            <span>CAISSE</span>
+        </div>
+    </div>
+    <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="white-box analytics-info">
+            <h5 class="box-title">SOLDE ESPECES</h5>
+            <ul class="list-inline two-part d-flex align-items-center mb-0">
+                <li class="ml-auto"><span class="counter text-info">
+                        <?= number_format($SoldeGlobal, 0, '.', '.'); ?>
+                    </span>
+                </li>
+            </ul>
+            <span>CAISSE</span>
+        </div>
+    </div>
+    <div class="col-lg-3 col-sm-6 col-xs-12">
+        <div class="white-box analytics-info">
+            <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Africa%2FBamako" width="100%" height="90" frameborder="0" seamless></iframe>
+        </div>
+    </div>
+</div>
+<?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier' or $_SESSION['statut'] == 'admin') { ?>
 
- <?php foreach ($links as $key => $name) { ?>
- <a href="<?= $name['url']; ?>" <?php if ($name['target'] == 1) { ?> target="_blank" <?php } ?>
-     class="btn btn-<?= $name['btn']; ?> mt-1"><i class="fa-solid fa-link"></i>
-     <?= $name['url_name']; ?></a>
- <?php } ?>
- <?php } ?>
- &nbsp;
+    <?php foreach ($links as $key => $name) { ?>
+        <a href="<?= $name['url']; ?>" <?php if ($name['target'] == 1) { ?> target="_blank" <?php } ?> class="btn btn-<?= $name['btn']; ?> mt-1"><i class="fa-solid fa-link"></i>
+            <?= $name['url_name']; ?></a>
+    <?php } ?>
+<?php } ?>
+&nbsp;
 
- <div class="row">
-     <div class="col-md-12 col-lg-12 col-sm-12">
-         <div class="white-box">
+<div class="row">
+    <div class="col-md-12 col-lg-12 col-sm-12">
+        <div class="white-box">
 
-             <div class="d-md-flex mb-3">
-                 <h3 class="box-title mb-0">OPERATIONS DU <?= date('d-m-Y'); ?> </h3>
-             </div>
-             <div class="table-responsive">
-                 <table id="dataTable" class="display nowrap" cellspacing="0" width="100%">
-                     <thead>
-                         <tr>
-                             <th class="border-top-0">RECU</th>
-                             <th class="border-top-0">REF</th>
-                             <th class="border-top-0">AGENCE</th>
-                             <th class="border-top-0">PRODUIT</th>
-                             <th class="border-top-0">CAISSE</th>
-                             <th class="border-top-0">OPERATION</th>
-                             <th class="border-top-0">CLIENT</th>
-                             <th class="border-top-0">N°COMPTE</th>
-                             <th class="border-top-0">MONTANT</th>
-                             <th class="border-top-0">REMARQUE</th>
+            <div class="d-md-flex mb-3">
+                <h3 class="box-title mb-0">OPERATIONS DU <?= date('d-m-Y'); ?> </h3>
+            </div>
+            <div class="table-responsive">
+                <table id="dataTable" class="display nowrap" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th class="border-top-0">RECU</th>
+                            <th class="border-top-0">REF</th>
+                            <th class="border-top-0">AGENCE</th>
+                            <th class="border-top-0">PRODUIT</th>
+                            <th class="border-top-0">CAISSE</th>
+                            <th class="border-top-0">OPERATION</th>
+                            <th class="border-top-0">CLIENT</th>
+                            <th class="border-top-0">N°COMPTE</th>
+                            <th class="border-top-0">MONTANT</th>
+                            <th class="border-top-0">REMARQUE</th>
 
-                         </tr>
-                     </thead>
-                     <tbody>
-                         <?php foreach ($Operation as $key => $value) { ?>
-                         <tr class="advance-table-row">
-                             <td><a href="/bordereau/<?= $value['RefOperations']; ?>" target="_blank"
-                                     class="btn btn-primary" data-toggle="tooltip"
-                                     title="Cliquez ici pour imprimer le bordereau"><i class="fa fa-print"></i> </td>
-                             <td> <?= $value['RefOperations']; ?></td>
-                             <td> <?= $value['NameAgency']; ?></td>
-                             <td> <?= $value['NameProduit']; ?></td>
-                             <td> <?= $value['NameCaisse']; ?></td>
-                             <td><?= $value['NameType']; ?></td>
-                             <td><?= $value['NameClient']; ?></td>
-                             <td><?= $value['NumCompte']; ?></td>
-                             <td class="counter text-danger">
-                                 <?= number_format($value['MontantVersement'], 0, '.', '.'); ?></td>
-                             <td><?= $value['Remarque']; ?></td>
-                         </tr>
-                         <?php } ?>
-                     </tbody>
-                 </table>
-             </div>
-         </div>
-     </div>
- </div>
- <a href="/dashboard" target="_blank" class="btn btn-secondary" data-toggle="tooltip"
-     title="Cliquez ici pour voir les stats">Dashboard</a>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($Operation as $key => $value) { ?>
+                            <tr class="advance-table-row">
+                                <td><a href="/bordereau/<?= $value['RefOperations']; ?>" target="_blank" class="btn btn-primary" data-toggle="tooltip" title="Cliquez ici pour imprimer le bordereau"><i class="fa fa-print"></i> </td>
+                                <td> <?= $value['RefOperations']; ?></td>
+                                <td> <?= $value['NameAgency']; ?></td>
+                                <td> <?= $value['NameProduit']; ?></td>
+                                <td> <?= $value['NameCaisse']; ?></td>
+                                <td><?= $value['NameType']; ?></td>
+                                <td><?= $value['NameClient']; ?></td>
+                                <td><?= $value['NumCompte']; ?></td>
+                                <td class="counter text-danger">
+                                    <?= number_format($value['MontantVersement'], 0, '.', '.'); ?></td>
+                                <td><?= $value['Remarque']; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<a href="/dashboard" target="_blank" class="btn btn-secondary" data-toggle="tooltip" title="Cliquez ici pour voir les stats">Dashboard</a>
--- Web/scripts/getList.js
+++ Web/scripts/getList.js
@@ -25,14 +25,18 @@ $(function () {
                 fragment.appendChild($('<option value="">Agence</option>')[0]);
 
                 $.each(data, function (index, value) {
-                    fragment.appendChild($(`<option value="${index}">${value}</option>`)[0]);
+                    const $option = $(`<option value="${index}">${value}</option>`);
+                    // Set the "data-desired-agency" attribute for the desired option
+                    if ($option.val() === $agence.data('desired-agency')) {
+                        $option.attr('data-desired-agency', $agence.data('desired-agency'));
+                    }
+                    fragment.appendChild($option[0]);
                 });
-
                 // Replace options
                 $agence.empty().append(fragment);
 
                 // Check if the desired agency option is available and set it as selected
-                const desiredAgencyValue = 1;
+                const desiredAgencyValue = $agence.data('desired-agency');
                 const desiredAgencyOption = $agence.find(`option[value="${desiredAgencyValue}"]`);
                 if (desiredAgencyOption.length > 0) {
                     desiredAgencyOption.prop('selected', true);
@@ -48,11 +52,12 @@ $(function () {
             }
         });
     });
+
     // Update the available caisses when the selected agency changes
     $agence.on('change', function () {
         const val = $(this).val();
 
-        // Clear the caisse select when changing the agency
+        // Clear the caisse select when changing the agency select
         $caisse.empty();
 
         // Make an AJAX request to get the available caisses
@@ -66,14 +71,19 @@ $(function () {
                 fragment.appendChild($('<option value="">Caisse</option>')[0]);
 
                 $.each(data, function (index, value) {
-                    fragment.appendChild($(`<option value="${index}">${value}</option>`)[0]);
+                    const $option = $(`<option value="${index}">${value}</option>`);
+                    // Set the "data-desired-caisse" attribute for the desired option
+                    if ($option.val() === $caisse.data('desired-caisse')) {
+                        $option.attr('data-desired-caisse', $caisse.data('desired-caisse'));
+                    }
+                    fragment.appendChild($option[0]);
                 });
 
                 // Replace options
                 $caisse.empty().append(fragment);
 
                 // Check if the desired caisse option is available and set it as selected
-                const desiredCaisseValue = 1;
+                const desiredCaisseValue = $caisse.data('desired-caisse');
                 const desiredCaisseOption = $caisse.find(`option[value="${desiredCaisseValue}"]`);
                 if (desiredCaisseOption.length > 0) {
                     desiredCaisseOption.prop('selected', true);
@@ -89,7 +99,5 @@ $(function () {
             }
         });
     })
-        // Restore previous selection when the page is loaded
-        .trigger('change');
 });
 

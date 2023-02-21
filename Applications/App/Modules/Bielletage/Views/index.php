4c1a4039293966f632a2676f944a30213bd1f90e
dione24 <dioneniangaly@gmail.com>
2023-02-21 09:39:19 +0000

    Fixed

--- Applications/App/Modules/Bielletage/Views/index.php
+++ Applications/App/Modules/Bielletage/Views/index.php
@@ -1,26 +1,38 @@
-  <?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier') { ?>
   <div class="col-lg-12 col-sm-12 col-xs-12">
-      <?php foreach ($Agence as $key => $value) {
-                if ($value['SommeDepot'] ==  0) { ?>
-      <div class="alert alert-danger" role="alert">
-          <span class="badge badge-danger"><?= $value['NameAgency']; ?> | Appro</span> Le solde
-          de la reserve est : <?= number_format($value['YesterdayReserve'], 0, '.', '.'); ?> | Merci d'Approvisonner
-          l'Agence
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-          </button>
-      </div>
-      <?php  }
-            } ?>
+      <select class="form-control" id="selectAgence">
+          <option value="0">Toutes les agences</option>
+          <?php foreach ($Agence as $key => $value) { ?>
+              <option value="<?= $value['RefAgency']; ?>"><?= $value['NameAgency']; ?></option>
+          <?php } ?>
+      </select>
   </div>
+
+
+
+
+
+  <?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier') { ?>
+      <div class=" col-lg-12 col-sm-12 col-xs-12">
+          <?php foreach ($Agence as $key => $value) {
+                if ($value['SommeDepot'] ==  0) { ?>
+                  <div class="alert alert-danger" role="alert">
+                      <span class="badge badge-danger"><?= $value['NameAgency']; ?> | Appro</span> Le solde
+                      de la reserve est : <?= number_format($value['YesterdayReserve'], 0, '.', '.'); ?> | Merci d'Approvisonner
+                      l'Agence
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+          <?php  }
+            } ?>
+      </div>
   <?php } ?>
   <div class="row justify-content-center">
       <div class="col-lg-3 col-sm-6 col-xs-12">
           <div class="white-box analytics-info">
               <h3 class="box-title">DEPOT</h3>
               <ul class="list-inline two-part d-flex align-items-center mb-0">
-                  <li class="ml-auto"><span
-                          class="counter text-danger"><?= number_format($SommeVersementGlobal, 0, '.', '.'); ?></span>
+                  <li class="ml-auto"><span class="counter text-danger"><?= number_format($SommeVersementGlobal, 0, '.', '.'); ?></span>
                   </li>
               </ul>
               <span>CAISSE</span>
@@ -30,8 +42,7 @@
           <div class="white-box analytics-info">
               <h3 class="box-title">RETRAIT</h3>
               <ul class="list-inline two-part d-flex align-items-center mb-0">
-                  <li class="ml-auto"><span
-                          class="counter text-purple"><?= number_format($SommeRetraitGlobal, 0, '.', '.'); ?></span>
+                  <li class="ml-auto"><span class="counter text-purple"><?= number_format($SommeRetraitGlobal, 0, '.', '.'); ?></span>
                   </li>
               </ul>
               <span>CAISSE</span>
@@ -51,19 +62,16 @@
       </div>
       <div class="col-lg-3 col-sm-6 col-xs-12">
           <div class="white-box analytics-info">
-              <iframe
-                  src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Africa%2FBamako"
-                  width="100%" height="90" frameborder="0" seamless></iframe>
+              <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=small&timezone=Africa%2FBamako" width="100%" height="90" frameborder="0" seamless></iframe>
           </div>
       </div>
   </div>
   <?php if ($_SESSION['statut'] == 'ChefCaisse' or $_SESSION['statut'] == 'Caissier' or $_SESSION['statut'] == 'admin') { ?>
 
-  <?php foreach ($links as $key => $name) { ?>
-  <a href="<?= $name['url']; ?>" <?php if ($name['target'] == 1) { ?> target="_blank" <?php } ?>
-      class="btn btn-<?= $name['btn']; ?> mt-1"><i class="fa-solid fa-link"></i>
-      <?= $name['url_name']; ?></a>
-  <?php } ?>
+      <?php foreach ($links as $key => $name) { ?>
+          <a href="<?= $name['url']; ?>" <?php if ($name['target'] == 1) { ?> target="_blank" <?php } ?> class="btn btn-<?= $name['btn']; ?> mt-1"><i class="fa-solid fa-link"></i>
+              <?= $name['url_name']; ?></a>
+      <?php } ?>
   <?php } ?>
   </br> </br>
 
@@ -93,21 +101,19 @@
                       </thead>
                       <tbody>
                           <?php foreach ($Operation as $key => $value) { ?>
-                          <tr class="advance-table-row">
-                              <td><a href="/bordereau/<?= $value['RefOperations']; ?>" target="_blank"
-                                      class="btn btn-primary" data-toggle="tooltip"
-                                      title="Cliquez ici pour imprimer le bordereau"><i class="fa fa-print"></i> </td>
-                              <td> <?= $value['RefOperations']; ?></td>
-                              <td> <?= $value['NameAgency']; ?></td>
-                              <td> <?= $value['NameProduit']; ?></td>
-                              <td> <?= $value['NameCaisse']; ?></td>
-                              <td><?= $value['NameType']; ?></td>
-                              <td><?= $value['NameClient']; ?></td>
-                              <td><?= $value['NumCompte']; ?></td>
-                              <td class="counter text-danger">
-                                  <?= number_format($value['MontantVersement'], 0, '.', '.'); ?></td>
-                              <td><?= $value['Remarque']; ?></td>
-                          </tr>
+                              <tr class="advance-table-row">
+                                  <td><a href="/bordereau/<?= $value['RefOperations']; ?>" target="_blank" class="btn btn-primary" data-toggle="tooltip" title="Cliquez ici pour imprimer le bordereau"><i class="fa fa-print"></i> </td>
+                                  <td> <?= $value['RefOperations']; ?></td>
+                                  <td> <?= $value['NameAgency']; ?></td>
+                                  <td> <?= $value['NameProduit']; ?></td>
+                                  <td> <?= $value['NameCaisse']; ?></td>
+                                  <td><?= $value['NameType']; ?></td>
+                                  <td><?= $value['NameClient']; ?></td>
+                                  <td><?= $value['NumCompte']; ?></td>
+                                  <td class="counter text-danger">
+                                      <?= number_format($value['MontantVersement'], 0, '.', '.'); ?></td>
+                                  <td><?= $value['Remarque']; ?></td>
+                              </tr>
                           <?php } ?>
                       </tbody>
                   </table>
@@ -115,5 +121,4 @@
           </div>
       </div>
   </div>
-  <a href="/dashboard" target="_blank" class="btn btn-secondary" data-toggle="tooltip"
-      title="Cliquez ici pour voir les stats">Dashboard</a>
+  <a href="/dashboard" target="_blank" class="btn btn-secondary" data-toggle="tooltip" title="Cliquez ici pour voir les stats">Dashboard</a>

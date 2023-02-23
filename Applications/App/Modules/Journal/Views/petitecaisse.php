9b61b88991bf49a40fc476cbb79dac954774b416
dione24 <dioneniangaly@gmail.com>
2023-02-23 09:56:26 +0000

    Fixed

--- Applications/App/Modules/Journal/Views/petitecaisse.php
+++ Applications/App/Modules/Journal/Views/petitecaisse.php
@@ -143,55 +143,37 @@
                                   <?php } ?>
                               </td>
                               <?php } ?>
-                              <div class="modal fade" id="depotModal-<?= $value['RefAgency']; ?>" tabindex="-1"
-                                  role="dialog" aria-labelledby="AddCaisse">
-                                  <div class="modal-dialog" role="document">
-                                      <div class="modal-content">
-                                          <div class="modal-header">Volume Dépôt - Retrait Par Produit -
-                                              <?= $print['NameCaisse']; ?> - <?= $value['NameAgency']; ?>
-                                              <button type="button" class="close" data-dismiss="modal"
-                                                  aria-label="Close">
-                                                  <span aria-hidden="true">&times;</span>
+                              <div class='modal fade' id='depotModal-<?= $value['RefAgency']; ?>' tabindex='-1'
+                                  role='dialog' aria-labelledby='AddCaisse'>
+                                  <div class='modal-dialog' role='document'>
+                                      <div class='modal-content'>
+                                          <div class='modal-header'>Volume Dépôt - Retrait Par Produit
+                                              -<?= $value['NameAgency']; ?>
+                                              <button type='button' class='close' data-dismiss='modal'
+                                                  aria-label='Close'>
+                                                  <span aria-hidden='true'>&times;</span>
                                               </button>
                                           </div>
-                                          <div class="modal-body">
+                                          <div class='modal-body'>
                                               <ul>
-                                                  <h5>Dépôt</h5>
-                                                  <?php foreach ($value['SommeDepotProduit'] as $product => $total) { ?>
-                                                  <li><?php echo $product; ?> :
-                                                      <?php
-                                                                if (is_numeric($total)) {
-                                                                    echo number_format($total, 0, '.', '.');
-                                                                } else {
-                                                                    echo $total === null ? '0' : '0';
-                                                                }
-
-                                                                ?>
+                                                  <?php foreach ([$value['SommeDepotProduit'], $value['SommeSortieProduit']] as $index => $products) : ?>
+                                                  <h5><?= $index === 0 ? 'Dépôt' : 'Retrait' ?></h5>
+                                                  <?php foreach ($products as $product => $total) : ?>
+                                                  <li><?= $product ?> :
+                                                      <?= is_numeric($total) ? number_format($total, 0, '.', '.') : ($total ?? '0') ?>
                                                   </li>
-                                                  <?php } ?>
-                                              </ul>
-                                              <hr>
-                                              <ul>
-                                                  <h5>Retrait</h5>
-                                                  <?php foreach ($value['SommeRetraitProduit'] as $product => $total) { ?>
-                                                  <li><?php echo $product; ?> :
-                                                      <?php
-                                                                if (is_numeric($total)) {
-                                                                    echo number_format($total, 0, '.', '.');
-                                                                } else {
-                                                                    echo $total === null ? '0' : '0';
-                                                                } ?>
-                                                  </li>
-                                                  <?php } ?>
+                                                  <?php endforeach; ?>
+                                                  <?php endforeach; ?>
                                               </ul>
                                           </div>
-                                          <div class="modal-footer">
-                                              <button type="button" class="btn btn-danger"
-                                                  data-dismiss="modal">Fermer</button>
+                                          <div class='modal-footer'>
+                                              <button type='button' class='btn btn-danger'
+                                                  data-dismiss='modal'>Fermer</button>
                                           </div>
                                       </div>
                                   </div>
                               </div>
+
                           </tr>
 
                           <?php } ?>

617d4b314a4a340be0abd33eb7682be1b5e9bd02
dione24 <dioneniangaly@gmail.com>
2023-02-28 15:51:26 +0000

    Fixed

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -13,7 +13,7 @@
     <span id="statut"></span>
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
-    <br>
+    &nbsp;
     <button class="btn btn-lg btn-info btn-block" type="submit" id="register">Connexion</button>
     <p class="mt-5 mb-3 text-muted">&copy; AFRIK CREANCES <?= date('Y'); ?> Developed by NIANGALY</p>
 </form>

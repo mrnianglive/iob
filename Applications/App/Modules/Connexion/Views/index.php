cf3fdb0878be175a21000280c8e088c2a67e753f
dione24 <dioneniangaly@gmail.com>
2023-02-28 15:49:01 +0000

    ddd

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -4,7 +4,8 @@
 <form method="post">
     <img src="/images/afc.png" alt="afc" width="200" height="100%">
     <div class="text-center social-btn">
-        <a href="#" class="btn btn-warning btn-block"><i class="fa fa-microsoft"></i> Sign in with <b>Microsoft</b></a>
+        <a href="#" class="btn btn-warning btn-block"><i class="fa-brands fa-microsoft"></i> Sign in with
+            <b>Microsoft</b></a>
     </div>
     <div class="or-seperator"><i>or</i></div>
     <label for="inputEmail" class="sr-only">Login</label>

866a2b8a7c0e0a6b75d77a2c4975a78c4ab70dfd
dione24 <dioneniangaly@gmail.com>
2023-03-01 08:22:04 +0000

    oka

--- Library/Models/UserManagerPDO.class.php
+++ Library/Models/UserManagerPDO.class.php
@@ -304,15 +304,5 @@ class UserManagerPDO extends UserManager
 
     public function AuthMicrosoft()
     {
-        $tenant = "common";
-        $client_id = "f6e12fd3-c628-4694-9c44-290e89e71543";
-        $client_secret = "s_cB1Aj-0I.0XWa3eVJ6SSf~1EQS3E5r~3";
-        $callback = "http://localhost:8080/callback.php";
-        $scopes = ["User.Read"];
-
-        $callback = "https://iob.malicreances-sa.com/callback.php";
-
-        $microsoft = new Auth($tenant, $client_id, $client_secret, $callback, $scopes);
-        header("location: " . $microsoft->getAuthUrl());
     }
 }

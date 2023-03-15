425fa5bc4c6f71bae49d18bb4c93c68f4cf78436
dione24 <dioneniangaly@gmail.com>
2023-03-15 10:18:46 +0000

    Removing Developed

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -16,5 +16,5 @@
     <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
     &nbsp;
     <button class="btn btn-lg btn-info btn-block" type="submit" id="register">Connexion</button>
-    <p class="mt-5 mb-3 text-muted">&copy; AFRIK CREANCES <?= date('Y'); ?> Developed by NIANGALY</p>
+    <p class="mt-5 mb-3 text-muted">&copy; AFRIK CREANCES <?= date('Y'); ?></p>
 </form>
--- Applications/App/Templates/layout.php
+++ Applications/App/Templates/layout.php
@@ -243,8 +243,7 @@ if (!isset($_SESSION['DoubleAuth']) && isset($_SESSION['secret'])) {
             <footer class="footer text-center">
                 <?= $_SESSION['nomPays'] ?? 'AFC'; ?> |
                 <?= date('Y'); ?>
-                © <a href="https://afrikcreances.com" target="_blank">AFRIK CREANCES</a> CONNCEPTION BY <a
-                    href="https://niangaly.com" target="_blank">NIANGALY</a>
+                © <a href="https://afrikcreances.com" target="_blank">AFRIK CREANCES</a>
             </footer>
             <!-- ============================================================== -->
             <!-- End footer -->

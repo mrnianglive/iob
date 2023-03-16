af3da0d0111fb8abb1dd11bc8e6d668a1cf0501a
dione24 <dioneniangaly@gmail.com>
2023-03-16 08:56:20 +0000

    Update

--- Applications/App/Modules/Connexion/Views/index.php
+++ Applications/App/Modules/Connexion/Views/index.php
@@ -1,5 +1,5 @@
 <?php if ($user->hasFlash()) { ?>
-    <p><?= $user->getFlash(); ?></p>
+<p><?= $user->getFlash(); ?></p>
 <?php } ?>
 <form method="post">
     <img src="/images/afc.png" alt="afc" width="200" height="100%">
@@ -10,7 +10,7 @@
     </div>
     <div class="or-seperator"><i>or</i></div> -->
     <label for="inputEmail" class="sr-only">Login</label>
-    <input type="text" id="login" class="form-control mb-1" name="login" placeholder="Login" required autofocus>
+    <input type="text" id="login" class="form-control" name="login" placeholder="Login" required autofocus>
     <span id="statut"></span>
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>

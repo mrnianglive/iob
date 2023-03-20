68eeeb9bc3adc093411a7da875d9fd3110732bb0
dione24 <dioneniangaly@gmail.com>
2023-03-20 12:57:05 +0000

    DONE

--- Applications/App/Modules/Bielletage/Views/invoice.php
+++ Applications/App/Modules/Bielletage/Views/invoice.php
@@ -291,9 +291,9 @@
                  </div>
              </div>
          </div>
-         <p style="text-align:right;">
+         <h5 style="text-align:right;">
              <?php if ($GetInvoice['RefPays'] == 1) { ?>MALI CREANCES SA - Intermediare en Opérations de Banque et
              Recouvrement |www.malicreances-sa.com<?php } else { ?>AFRIK CREANCES - Intermédiation en opérations
              bancaires et non bancaires |www.afrikcreances.com<?php } ?>
-         </p>
+         </h5>
      </div>

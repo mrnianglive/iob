c398780713764dd3fec3ea79dee67835edf5fea3
dione24 <dioneniangaly@gmail.com>
2023-03-20 14:41:24 +0000

    Fixed

--- Library/Models/ArreterManagerPDO.class.php
+++ Library/Models/ArreterManagerPDO.class.php
@@ -290,104 +290,15 @@ class ArreterManagerPDO extends ArreterManager
         }
     }
 
-    function NumberToLetter($number, $uppercase = false, $lang = 'fr')
+    function NumberToLetter($nombre, $uppercase = false)
     {
-        $toLetter = [
-            0 => "zéro",
-            1 => "un",
-            2 => "deux",
-            3 => "trois",
-            4 => "quatre",
-            5 => "cinq",
-            6 => "six",
-            7 => "sept",
-            8 => "huit",
-            9 => "neuf",
-            10 => "dix",
-            11 => "onze",
-            12 => "douze",
-            13 => "treize",
-            14 => "quatorze",
-            15 => "quinze",
-            16 => "seize",
-            17 => "dix-sept",
-            18 => "dix-huit",
-            19 => "dix-neuf",
-            20 => "vingt",
-            30 => "trente",
-            40 => "quarante",
-            50 => "cinquante",
-            60 => "soixante",
-            70 => "soixante-dix",
-            80 => "quatre-vingt",
-            90 => "quatre-vingt-dix",
-        ];
+        $fmt = new \NumberFormatter('fr', \NumberFormatter::SPELLOUT);
+        $result = $fmt->format($nombre);
 
-        $numberToLetter = '';
-        $nb = floatval($number);
-
-        if ($nb > 999999999) return "dépassement de capacité";
-        if (!is_numeric($number)) return "Nombre non valide";
-        if (ceil($nb) != $nb) {
-            $nb = explode('.', $number);
-            return NumberToLetter($nb[0]) . " virgule " . NumberToLetter($nb[1]);
-        }
-
-        if ($nb < 0) {
-            $numberToLetter .= 'moins ';
-            $nb *= -1;
-        }
-
-        $n = strlen($number);
-        switch ($n) {
-            case 1:
-                $numberToLetter .= $toLetter[$nb];
-                break;
-            case 2:
-                if ($nb > 19) {
-                    $quotient = floor($nb / 10);
-                    $reste = $nb % 10;
-                    if ($nb < 71 || ($nb > 79 && $nb < 91)) {
-                        if ($reste == 0) $numberToLetter .= $toLetter[$quotient * 10];
-                        if ($reste == 1) $numberToLetter .= $toLetter[$quotient * 10] . "-et-" . $toLetter[$reste];
-                        if ($reste > 1) $numberToLetter .= $toLetter[$quotient * 10] . "-" . $toLetter[$reste];
-                    } else $numberToLetter .= $toLetter[($quotient - 1) * 10] . "-" . $toLetter[10 + $reste];
-                } else $numberToLetter .= $toLetter[$nb];
-                break;
-
-            case 3:
-                $quotient = floor($nb / 100);
-                $reste = $nb % 100;
-                if ($quotient == 1 && $reste == 0) $numberToLetter .= "cent";
-                if ($quotient == 1 && $reste != 0) $numberToLetter .= "cent" . " " . NumberToLetter($reste);
-                if ($quotient > 1 && $reste == 0) $numberToLetter($quotient) . " cents";
-                if ($quotient > 1 && $reste != 0) $numberToLetter .= NumberToLetter($quotient) . " cent " . NumberToLetter($reste);
-                break;
-            case 4:
-            case 5:
-            case 6:
-                $quotient = floor($nb / 1000);
-                $reste = $nb - $quotient * 1000;
-                if ($quotient == 1 && $reste == 0) $numberToLetter .= "mille";
-                if ($quotient == 1 && $reste != 0) $numberToLetter .= "mille" . " " . NumberToLetter($reste);
-                if ($quotient > 1 && $reste == 0) $numberToLetter .= NumberToLetter($quotient) . " mille";
-                if ($quotient > 1 && $reste != 0) $numberToLetter .= NumberToLetter($quotient) . " mille " . NumberToLetter($reste);
-                break;
-            case 7:
-            case 8:
-            case 9:
-                $quotient = floor($nb / 1000000);
-                $reste = $nb % 1000000;
-                if ($quotient == 1 && $reste == 0) $numberToLetter .= "un million";
-                if ($quotient == 1 && $reste != 0) $numberToLetter .= "un million" . " " . NumberToLetter($reste);
-                if ($quotient > 1 && $reste == 0) $numberToLetter .= NumberToLetter($quotient) . " millions";
-                if ($quotient > 1 && $reste != 0) $numberToLetter .= NumberToLetter($quotient) . " millions " . NumberToLetter($reste);
-                break;
-        }
         if ($uppercase) {
-            $numberToLetter = ucfirst($numberToLetter);
+            $result = ucfirst($result);
         }
 
-        return $numberToLetter;
+        return $result;
     }
 }

8836c17803aaeb529a91fd94df2e53197462f12c
dione24 <dioneniangaly@gmail.com>
2023-03-22 08:56:16 +0000

    TEST

--- Applications/App/Modules/Bielletage/Views/invoice.php
+++ Applications/App/Modules/Bielletage/Views/invoice.php
@@ -13,7 +13,7 @@
                      <p>
                          <?php if ($GetInvoice['RefPays'] == 1) { ?>
                          MALI CREANCES SA
-                         <?= $GetInvoice['NameBanque'] ? ' - PARTENAIRE' . $GetInvoice['NameBanque'] : ''; ?>
+                         <?= $GetInvoice['NameBanque'] ? ' - PARTENAIRE ' . $GetInvoice['NameBanque'] : ''; ?>
                          <?php } else { ?>
                          AFRIK CREANCES
                          <?php } ?>
@@ -75,7 +75,7 @@
 
                      <p>DATE DE VALEUR : <?= date('d-M-Y', strtotime($GetInvoice['Insert_Time'])); ?></p>
                      <p class="text-uppercase">REMARQUES : <?= $GetInvoice['Remarque']; ?></p>
-                     <p class="text-uppercase">CAISSIER(E) : <?= $GetInvoice['login']; ?></p>
+                     <p class="text-uppercase">CAISSIER(E) : <?= $GetInvoice['PrenomUsers']; ?></p>
                  </div>
                  <div style="width: 48%;display: inline-block;vertical-align: top;">
                      <div style="border: 1px solid grey;border-radius: 4px;padding: 4px 8px;margin-bottom: 10px;">
@@ -223,7 +223,7 @@
 
                      <p>DATE DE VALEUR : <?= date('d-M-Y', strtotime($GetInvoice['Insert_Time'])); ?></p>
                      <p class="text-uppercase">REMARQUES : <?= $GetInvoice['Remarque']; ?></p>
-                     <p class="text-uppercase">CAISSIER(E) : <?= $GetInvoice['login']; ?></p>
+                     <p class="text-uppercase">CAISSIER(E) : <?= $GetInvoice['PrenomUsers']; ?></p>
                  </div>
                  <div style="width: 48%;display: inline-block;vertical-align: top;">
                      <div style="border: 1px solid grey;border-radius: 4px;padding: 4px 8px;margin-bottom: 10px;">

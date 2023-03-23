0618e7d3fc9f770e665c9b0b2449d398add56a13
dione24 <dioneniangaly@gmail.com>
2023-03-23 13:34:43 +0000

    Back to default

--- Applications/App/Modules/Bielletage/Views/invoice.php
+++ Applications/App/Modules/Bielletage/Views/invoice.php
@@ -4,7 +4,7 @@
                  style="border: 1px solid grey;border-radius: 4px;padding: 4px; background-color: #efefef;">
                  <div style="width: 10%; display: inline-block; vertical-align: top;">
                      <?php if ($GetInvoice['RefPays'] == 1) { ?>
-                     <img src="/bordereau/mlc.jpg" alt="Logo" width="80" height="80">
+                     <img src="/images/mlc.png" alt="Logo" width="80" height="80">
                      <?php } else { ?>
                      <img src="/images/afc.png" alt="Logo" width="80" height="80">
                      <?php } ?>
@@ -147,7 +147,7 @@
                  style="border: 1px solid grey;border-radius: 4px;padding: 4px; background-color: #efefef;">
                  <div style="width: 10%; display: inline-block; vertical-align: top;">
                      <?php if ($GetInvoice['RefPays'] == 1) { ?>
-                     <img src="/bordereau/mlc.jpg" alt="Logo" width="80" height="80">
+                     <img src="/images/mlc.png" alt="Logo" width="80" height="80">
                      <?php } else { ?>
                      <img src="/images/afc.png" alt="Logo" width="80" height="80">
                      <?php } ?>

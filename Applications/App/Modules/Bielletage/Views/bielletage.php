3cdd5aea8bfe4c0123e01f0ccfa7f9619d21952b
dione24 <dioneniangaly@gmail.com>
2023-04-13 13:48:52 +0000

    default

--- Applications/App/Modules/Bielletage/Views/bielletage.php
+++ Applications/App/Modules/Bielletage/Views/bielletage.php
@@ -19,12 +19,6 @@
                 Caisse <?php } elseif ($_GET['id'] == 4) { ?>Sortie de
                 Fond<?php } elseif ($_GET['id'] == 5) { ?>Transfert Caisse2Caisse <?php } ?> </a>
         </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#step-4">
-                Selectionner le partenaire
-            </a>
-        </li>
     </ul>
     <form method="POST" action='/bielletage/add'>
         <br />
@@ -391,7 +385,6 @@
 
             </div>
 
-
         </div>
 
         <?php } else { ?>
@@ -659,35 +652,5 @@
 
         </div>
         <?php } ?>
-
-        <div id="step-4" class="tab-pane" role="tabpanel" aria-labelledby="step-4">
-
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-4">
-                    <a href="https://www.example.com/produit1">
-                        <img src="https://via.placeholder.com/150" alt="Produit 1"
-                            class="rounded-circle circle-img mx-auto mb-2">
-                        <h5 class="mb-0">Produit 1</h5>
-                    </a>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <a href="https://www.example.com/produit2">
-                        <img src="https://via.placeholder.com/150" alt="Produit 2"
-                            class="rounded-circle circle-img mx-auto mb-2">
-                        <h5 class="mb-0">Produit 2</h5>
-                    </a>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <a href="https://www.example.com/produit3">
-                        <img src="https://via.placeholder.com/150" alt="Produit 3"
-                            class="rounded-circle circle-img mx-auto mb-2">
-                        <h5 class="mb-0">Produit 3</h5>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-
-
     </form>
 </div>

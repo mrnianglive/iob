b59e112448511f2caef6e01a079adeb9e3c548f4
dione24 <dioneniangaly@gmail.com>
2023-04-17 11:37:16 +0000

    Fixed

--- Web/config/requetelisteJournal.php
+++ Web/config/requetelisteJournal.php
@@ -2,10 +2,10 @@
 
 require("db.php");
 
-if (isset($_GET['RefAgency'])) {
+if (isset($_GET['Caisse'])) {
     $tableau = array();
     $requete = $baseDeDonnee->prepare('SELECT * FROM TbleProduit INNER JOIN TbleChmodProduit ON TbleChmodProduit.RefProduit=TbleProduit.RefProduit INNER JOIN TbleCaisse ON TbleCaisse.RefCaisse=TbleChmodProduit.RefCaisse WHERE TbleCaisse.RefAgency=:RefAgency GROUP BY TbleProduit.RefProduit');
-    $requete->bindValue(':RefAgency', $_GET['RefAgency'], PDO::PARAM_INT);
+    $requete->bindValue(':RefAgency', $_GET['Caisse'], PDO::PARAM_INT);
     $requete->execute();
     $resultat = $requete->fetchAll();
     foreach ($resultat as $key => $value) {

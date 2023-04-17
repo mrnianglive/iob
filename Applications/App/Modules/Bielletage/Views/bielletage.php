789a4b891b80d5f2cef80b6e7977d66483b0bfef
dione24 <dioneniangaly@gmail.com>
2023-04-17 09:59:26 +0000

    Done

--- Applications/App/Modules/Bielletage/Views/bielletage.php
+++ Applications/App/Modules/Bielletage/Views/bielletage.php
@@ -43,7 +43,7 @@
 
                     <div class="col-md-6">
                         <div class="row">
-                            <div class="col-sm-2">
+                            <div class="col-md-6">
                                 <div class="form-group">
                                     <label class="control-label">Code Produit</label>
                                     <input type="int" id="a2" class="form-control number-input" name="a2" autocomplete="OFF" style="border: 1px solid coral;">

ce75b8633cdba50efef50f44cd10a6798409479a
dione24 <dioneniangaly@gmail.com>
2023-04-17 10:45:16 +0000

    Test

--- Web/scripts/produitlist.js
+++ Web/scripts/produitlist.js
@@ -11,15 +11,14 @@ $(function () {
             success: function (json) {
                 $.each(json, function (index, value) {
                     var produitHtml = `
-                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="RefProduit" id="produit${index}" value="${index}">
-                            <label class="form-check-label d-block bg-light p-3 rounded-circle" for="produit${index}">
-                                <div class="circle-img">
-                                    <img src="${value.image}" width="150" height="150" alt="${value.nom}" class="mx-auto mb-2">
-                                </div>
-                            </label>
+                    <div class="form-check d-inline mx-2">
+                        <input class="form-check-input" type="radio" name="RefProduit" id="produit${index}" value="${index}">
+                        <label class="form-check-label d-block bg-light p-3 rounded-circle" for="produit${index}">
+                        <div class="circle-img">
+                            <img src="${value.image}" alt="${value.nom}" class="mx-auto mb-2 produit-img">
                         </div>
-                      
+                        </label>
+                    </div>
                     `;
                     $liste.append(produitHtml);
                 });

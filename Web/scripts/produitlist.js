25af85572354ae87e0b4d0160a3b5fda4673eb17
dione24 <dioneniangaly@gmail.com>
2023-04-17 10:08:31 +0000

    Test

--- Web/scripts/produitlist.js
+++ Web/scripts/produitlist.js
@@ -10,11 +10,20 @@ $(function () {
             dataType: 'json',
             success: function (json) {
                 $.each(json, function (index, value) {
-                    $liste.append('<option value="' + index + '">' + value + '</option>');
+                    var produitHtml = `
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="produit" id="produit${index}" value="${index}">
+                            <label class="form-check-label d-block bg-light p-3 rounded-circle" for="produit${index}">
+                                <div class="circle-img">
+                                    <img src="https://via.placeholder.com/150" alt="${value}" class="mx-auto mb-2">
+                                </div>
+                                <h5 class="mb-0">${value}</h5>
+                            </label>
+                        </div>
+                    `;
+                    $liste.append(produitHtml);
                 });
             }
         });
-
-    })
-
+    });
 });

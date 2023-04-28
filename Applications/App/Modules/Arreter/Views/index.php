b85f0ba8374b77c59012fe3bfa385ebe0520abce
dione24 <dioneniangaly@gmail.com>
2023-04-28 14:50:22 +0000

    Fixed

--- Applications/App/Modules/Arreter/Views/index.php
+++ Applications/App/Modules/Arreter/Views/index.php
@@ -13,17 +13,30 @@
                       </thead>
                       <tbody>
                           <?php foreach ($Caisse as $key => $value) { ?>
-                              <tr>
-                                  <td><?= $value['NameCaisse']; ?></td>
-                                  <td><?= $value['NameAgency']; ?></td>
-                                  <td>
-                                      <?php if (!empty($value['Valide'])) { ?><a <?php if ($_SESSION['statut'] == 'superadmin' or $_SESSION['statut'] == 'admin') { ?> href="/Arreter/delete/<?= $value['Valide']['RefSolde']; ?>" <?php } ?> class="btn btn-success" data-toggle="tooltip" title="Cliquez ici pour annuler l'arrêté de Caisse"><i class="fa  fa-lock"></i></a>
-                                      <?php } else { ?><a <?php if ($_SESSION['statut'] == 'superadmin' or  $_SESSION['statut'] == 'admin' or $_SESSION['statut'] == 'ChefCaisse') { ?> href="/Arreter/close/<?= $value['RefCaisse']; ?>" <?php } ?> class="btn btn-danger" data-toggle="tooltip" title="Cliquez ici pour  arreter la caisse"><i class="fa fa-unlock"></i></a>
-                                      <?php } ?>
-                                      <a href="/Arreter/View/<?= $value['RefCaisse']; ?>" class="btn btn-primary" data-toggle="tooltip" title="Cliquez ici pour voir tous les arretes de Caisse"><i class="fa fa-eye"></i></a>
-                                  </td>
-                                  </td>
-                              </tr>
+                          <tr>
+                              <td><?= $value['NameCaisse']; ?></td>
+                              <td><?= $value['NameAgency']; ?></td>
+                              <td>
+                                  <?php if (!empty($value['Valide'])) { ?><a
+                                      <?php if ($_SESSION['statut'] == 'superadmin' or $_SESSION['statut'] == 'admin') { ?>
+                                      href="/Arreter/delete/<?= $value['Valide']['RefSolde']; ?>" <?php } ?>
+                                      class="btn btn-success" data-toggle="tooltip"
+                                      title="Cliquez ici pour annuler l'arrêté de Caisse"><i
+                                          class="fa  fa-lock"></i></a>
+                                  <?php } else { ?>
+                                  <?php if ($_SESSION['statut'] == 'superadmin' or  $_SESSION['statut'] == 'admin' or $_SESSION['statut'] == 'ChefCaisse') { ?>
+                                  <a href="/Arreter/close/<?= $value['RefCaisse']; ?>" <?php } ?> class="btn btn-danger"
+                                      data-toggle="tooltip" title="Cliquez ici pour  arreter la caisse"><i
+                                          class="fa fa-unlock"></i></a>
+                                  <?php } ?>
+
+
+                                  <a href="/Arreter/View/<?= $value['RefCaisse']; ?>" class="btn btn-primary"
+                                      data-toggle="tooltip" title="Cliquez ici pour voir tous les arretes de Caisse"><i
+                                          class="fa fa-eye"></i></a>
+                              </td>
+                              </td>
+                          </tr>
                           <?php } ?>
                       </tbody>
                   </table>

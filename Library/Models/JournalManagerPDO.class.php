8048a978657fb177635a079859ddd4be0451ed8f
dione24 <dioneniangaly@gmail.com>
2023-05-31 12:23:58 +0000

    done

--- Library/Models/JournalManagerPDO.class.php
+++ Library/Models/JournalManagerPDO.class.php
@@ -13,8 +13,8 @@ class JournalManagerPDO extends JournalManager
         $requete->bindValue(':RefBanque', $_SESSION['RefBanque'], \PDO::PARAM_INT);
         $requete->execute();
         $data = $requete->fetchAll();
-        if (empty($data)) {
-            $data = null;
+        if ($data == null) {
+            return 0;
         }
         return $data;
     }
@@ -29,8 +29,8 @@ class JournalManagerPDO extends JournalManager
             $data[$key]['Debut'] = $debut;
             $data[$key]['Debut'] = $fin;
         }
-        if (empty($data)) {
-            $data = null;
+        if ($data == null) {
+            return 0;
         }
         return $data;
     }

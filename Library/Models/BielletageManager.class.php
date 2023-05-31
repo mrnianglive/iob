4b87e4d3886b960e37736bd781987ae3bb3e2e08
dione24 <dioneniangaly@gmail.com>
2023-05-31 12:36:21 +0000

    Fixed

--- Library/Models/BielletageManager.class.php
+++ Library/Models/BielletageManager.class.php
@@ -6,18 +6,6 @@ use \Library\Entities\Bielletage;
 
 abstract class BielletageManager extends \Library\Manager
 {
-    abstract protected function CheckOuverture($data);
-    abstract protected function ChomdUser();
+
     abstract protected function GetInvoice($id);
-    abstract protected function GetAgency($Caisse);
-    abstract protected function CheckAfterRapport($Caisse);
-    abstract protected function Add();
-    abstract protected function SommeVersementCaisse($Date);
-    abstract protected function SommeRetraitCaisse($Date);
-    abstract protected function getCurrentWeek();
-    abstract  protected function daysofweek();
-    abstract protected function DailyVersement();
-    abstract protected function SommeRetraitStatistique($Date);
-    abstract protected function SommeVersementStatistique($Date);
-    abstract protected function YesterdaySolde($Caisse = NULL);
 }

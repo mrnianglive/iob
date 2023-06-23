fb5c48bea7dce47a5aebae7c0925804773bad74a
dione24 <dioneniangaly@gmail.com>
2023-06-23 11:02:09 +0000

    FIxed

--- Web/config/checklogin.php
+++ Web/config/checklogin.php
@@ -2,7 +2,7 @@
 require("db.php");
 
 if (isset($_GET['login'])) {
-    $query = $baseDeDonnee->prepare("SELECT * FROM TbleUsers WHERE login = '" . $_GET['login'] . "' ");
+    $query = $baseDeDonnee->prepare("SELECT * FROM TbleUsers WHERE login = '" . $_GET['login'] . " AND TbleUsers.RefStatut =8'");
     $query->execute();
     $data = $query->fetch();
     echo json_encode($data['login']);

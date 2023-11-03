780a2581ed9797dd9343d124dcfd070f93179666
dione24 <dioneniangaly@gmail.com>
2023-11-03 20:25:29 +0000

    Fix error message parameter in BielletageController class

--- Applications/App/Modules/Bielletage/BielletageController.class.php
+++ Applications/App/Modules/Bielletage/BielletageController.class.php
@@ -245,7 +245,7 @@ class BielletageController extends \Library\BackController
         // Valider le solde de la veille
         $balanceError = $this->ValidYesterdaySold($RefAgency, $Today);
         if ($balanceError) {
-            $redirectWithMessage('error', $balanceError, 3); // Utilisez le numéro d'erreur approprié
+            $redirectWithMessage('error', $balanceError, 3, $RefType); // Utilisez le nu)méro d'erreur approprié
             return;
         }
 

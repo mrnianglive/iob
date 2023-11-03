adbdd0afd1c292fb999ee30db27c3ba231a34f80
dione24 <dioneniangaly@gmail.com>
2023-11-03 12:04:44 +0000

    TEST VFF

--- Web/scripts/clientlastOp.js
+++ Web/scripts/clientlastOp.js
@@ -1,35 +1,31 @@
 $(function () {
     var $NumCompte = $('#NumCompte');
-    var $NameClient = $('#NameClient');
-    var $AlertMessage = $('#AlertMessage'); // Ajout d'une référence pour l'alerte
+    var $MontantVersement = $('#MontantVersement'); // Assurez-vous d'avoir un champ pour entrer le montant du versement
+    var $alertContainer = $('#alertContainer'); // Cet élément HTML affichera l'alerte
 
+    // Cette fonction sera appelée lorsque la valeur du champ NumCompte changera
     $NumCompte.on('change', function () {
-        var val = $(this).val();
-        $NameClient.empty();
-        $AlertMessage.empty(); // Vider le message précédent
-
-        if (val) {
+        var numCompteVal = $(this).val();
+        var montantVal = $MontantVersement.val(); // Récupère la valeur du montant du versement
+        if (numCompteVal) {
             $.ajax({
-                url: '/config/clientlastOp.php', // Assurez-vous que l'URL est correcte
-                data: { NumCompte: val }, // Envoyer les données comme un objet
+                url: '/config/clientlastOp.php',
+                type: 'GET',
+                data: {
+                    NumCompte: numCompteVal,
+                    MontantVersement: montantVal
+                },
                 dataType: 'json',
                 success: function (response) {
-                    if (response.nameClient) {
-                        $NameClient.val(response.nameClient);
-                    } else {
-                        $NameClient.val(''); // Vider le champ si aucune donnée n'est reçue
-                    }
-
-                    // Vérifier et afficher le message d'alerte si présent
                     if (response.message) {
-                        $AlertMessage.text(response.message).show();
+                        $alertContainer.text(response.message).show(); // Affiche le message dans le conteneur d'alerte
                     } else {
-                        $AlertMessage.hide();
+                        $alertContainer.hide(); // Cache le conteneur d'alerte si aucun message n'est retourné
                     }
                 },
                 error: function (xhr, status, error) {
-                    // Gérer l'erreur
-                    console.error("Erreur AJAX: " + status + " - " + error);
+                    console.error("An error occurred: " + error);
+                    $alertContainer.text("Erreur lors de la récupération de l'alerte.").show();
                 }
             });
         }

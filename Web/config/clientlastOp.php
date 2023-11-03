ae694aa6ce9ad11f3468432ab051e62f8c267a53
dione24 <dioneniangaly@gmail.com>
2023-11-03 11:47:41 +0000

    TEST

--- Web/config/clientlastOp.php
+++ Web/config/clientlastOp.php
@@ -10,26 +10,22 @@ if (isset($_GET['NumCompte']) && isset($_GET['MontantVersement'])) {
     $query = $baseDeDonnee->prepare("SELECT MontantVersement FROM TbleOperations WHERE NumCompte=:NumCompte AND TbleOperations.Approve2_Id IS NOT NULL AND TbleOperations.Reset_Id IS NULL ORDER BY DateTransaction DESC LIMIT 5");
     $query->bindValue(':NumCompte', $NumCompte, PDO::PARAM_INT);
     $query->execute();
-    $dataOperations = $query->fetchAll();
 
-    echo json_encode($dataOperations);
+    $recentTransactions = $query->fetchAll(PDO::FETCH_COLUMN, 0);
+    $averageRecentTransaction = array_sum($recentTransactions) / count($recentTransactions);
 
+    // Si le nouveau montant est, disons, 3 fois supérieur à la moyenne des transactions récentes, on génère une alerte
+    if ($newTransactionAmount > $averageRecentTransaction * 3) {
+        $response['message'] = "Alerte: Le client a effectué une transaction qui est considérablement plus élevée que ses 5 dernières transactions.";
+    }
 
-    // $recentTransactions = $query->fetchAll(PDO::FETCH_COLUMN, 0);
-    // $averageRecentTransaction = array_sum($recentTransactions) / count($recentTransactions);
+    // Récupérer le nom du client
+    $query = $baseDeDonnee->prepare("SELECT NameClient FROM TbleOperations WHERE NumCompte=:NumCompte LIMIT 1");
+    $query->bindValue(':NumCompte', $NumCompte, PDO::PARAM_INT);
+    $query->execute();
 
-    // // Si le nouveau montant est, disons, 3 fois supérieur à la moyenne des transactions récentes, on génère une alerte
-    // if ($newTransactionAmount > $averageRecentTransaction * 3) {
-    //     $response['message'] = "Alerte: Le client a effectué une transaction qui est considérablement plus élevée que ses 5 dernières transactions.";
-    // }
+    $data = $query->fetch();
+    $response['nameClient'] = $data['NameClient'];
 
-    // // Récupérer le nom du client
-    // $query = $baseDeDonnee->prepare("SELECT NameClient FROM TbleOperations WHERE NumCompte=:NumCompte LIMIT 1");
-    // $query->bindValue(':NumCompte', $NumCompte, PDO::PARAM_INT);
-    // $query->execute();
-
-    // $data = $query->fetch();
-    // $response['nameClient'] = $data['NameClient'];
-
-    // echo json_encode($response);
+    echo json_encode($NumCompte);
 }

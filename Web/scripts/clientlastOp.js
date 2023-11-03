0c4c0a4ea837be0ae6229a90ba0589fddf56bf46
dione24 <dioneniangaly@gmail.com>
2023-11-03 12:08:32 +0000

    TST

--- Web/scripts/clientlastOp.js
+++ Web/scripts/clientlastOp.js
@@ -10,7 +10,7 @@ $(function () {
         if (numCompteVal) {
             $.ajax({
                 url: '/config/clientlastOp.php',
-                type: 'post',
+                type: 'GET',
                 data: {
                     NumCompte: numCompteVal,
                     MontantVersement: montantVal

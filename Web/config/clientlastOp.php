9247d5c81a27436dc9439a6a1d606c7135f5d122
dione24 <dioneniangaly@gmail.com>
2023-11-03 11:57:05 +0000

    TEST

--- Web/config/clientlastOp.php
+++ Web/config/clientlastOp.php
@@ -1,28 +1,36 @@
 <?php
 require("db.php");
 
-if (isset($_GET['NumCompte'])) {
-    $montant = $_GET['MontantVersement'];
-    $query = $baseDeDonnee->prepare("SELECT * FROM TbleOperations WHERE NumCompte=:NumCompte ");
-    $query->bindValue(':NumCompte', $_GET['NumCompte'], PDO::PARAM_INT);
-    $query->execute();
-    $data = $query->fetch();
+$response = ['message' => '']; // Structure de réponse initiale
 
-    //Faire la moyenne des opérations du client et le comparer au versement en cours MontantVersmet
-    //Si le montant est supérieur à la moyenne, on  reourne un message d'alerte 
-    $requete = $baseDeDonnee->prepare("SELECT AVG(MontantVersement) FROM TbleOperations WHERE NumCompte=:NumCompte AND  AND TbleOperations.Approve2_Id IS NOT NULL AND TbleOperations.Reset_Id IS NULL ORDER BY DateTransaction DESC LIMIT 5");
-    $requete->bindValue(':NumCompte', $_GET['NumCompte'], PDO::PARAM_INT);
+if (isset($_GET['NumCompte'], $_GET['MontantVersement'])) {
+    $NumCompte = $_GET['NumCompte'];
+    $montant = (float) $_GET['MontantVersement']; // Cast pour s'assurer que c'est un nombre
+
+    // Calculer la moyenne des 5 derniers versements
+    $requete = $baseDeDonnee->prepare(
+        "SELECT AVG(MontantVersement) as moyenne 
+         FROM TbleOperations 
+         WHERE NumCompte=:NumCompte AND Approve2_Id IS NOT NULL AND Reset_Id IS NULL 
+         ORDER BY Approve2_Time DESC 
+         LIMIT 5"
+    );
+    $requete->bindValue(':NumCompte', $NumCompte, PDO::PARAM_INT);
     $requete->execute();
-    $moyenne = $requete->fetch();
+    $resultat = $requete->fetch(PDO::FETCH_ASSOC);
+
+    $moyenne = $resultat ? $resultat['moyenne'] : 0; // Vérifier si le résultat est non nul avant d'accéder à la clé
 
     if ($montant > $moyenne) {
-        //"Alerte: Le client a effectué une transaction qui est considérablement plus élevée que ses 5 dernières transactions.";
-        echo json_encode("Alerte: Le client a effectué une transaction qui est considérablement plus élevée que ses 5 dernières transactions.");
+        $response['message'] = "Alerte: Le client a effectué une transaction qui est considérablement plus élevée que ses 5 dernières transactions.";
     } else {
-        echo json_encode("Le montant est inférieur à la moyenne, pas d'alerte");
+        $response['message'] = "Le montant est inférieur à la moyenne, pas d'alerte.";
     }
-
-
-
-    echo json_encode($data['NameClient']);
+} else {
+    $response['message'] = "Erreur: Les données nécessaires ne sont pas fournies.";
 }
+
+// Encoder la réponse en JSON
+echo json_encode($response);
+
+// Fermer la connexion à la base de données

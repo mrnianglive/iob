b7b840f94290f0a67adbf2dd3b492143d892380f
dione24 <dioneniangaly@gmail.com>
2023-11-03 15:58:35 +0000

    Fixed

--- Web/config/clientlastOp.php
+++ Web/config/clientlastOp.php
@@ -5,6 +5,7 @@ $response = ['message' => '']; // Structure de réponse initiale
 if (isset($_GET['NumCompte'], $_GET['MontantVersement'])) {
     $NumCompte = $_GET['NumCompte'];
     $montant = (float) $_GET['MontantVersement']; // Cast pour s'assurer que c'est un nombre
+
     // Calculer la moyenne des 5 derniers versements
     $requete = $baseDeDonnee->prepare(
         "SELECT AVG(MontantVersement) as moyenne 
@@ -16,9 +17,10 @@ if (isset($_GET['NumCompte'], $_GET['MontantVersement'])) {
     $requete->bindValue(':NumCompte', $NumCompte, PDO::PARAM_INT);
     $requete->execute();
     $resultat = $requete->fetch(PDO::FETCH_ASSOC);
-    // Vérifier si le résultat est non nul avant d'accéder à la clé
-    if ($resultat) {
-        $moyenne = $resultat['moyenne'];
+
+    // Vérifier si une moyenne a été calculée
+    if ($resultat && $resultat['moyenne'] !== null) {
+        $moyenne = (float) $resultat['moyenne'];
         if ($montant > $moyenne) {
             $response['message'] = "Alerte: Transaction inhabituelle détectée! Le montant est supérieur à la moyenne des transactions précédentes.";
         } else {
@@ -26,13 +28,13 @@ if (isset($_GET['NumCompte'], $_GET['MontantVersement'])) {
         }
     } else {
         // Aucune transaction précédente trouvée - cela pourrait être un nouveau client
-        $response['message'] = "Avis: Pas d'historique de transaction. Cela pourrait être un nouveau client.";
+        $response['message'] = "Avis: Pas d'historique de transaction. Veuillez procéder à une vérification manuelle pour un nouveau client.";
     }
 } else {
     $response['message'] = "Erreur: Les données nécessaires pour effectuer l'analyse ne sont pas fournies.";
 }
+
 // Encoder la réponse en JSON
 echo json_encode($response);
-
-
-// Fermer la connexion à la base de données
+// Fermer la connexion à la base de données, si nécessaire
+// $baseDeDonnee = null;

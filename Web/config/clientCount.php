f07b068e8edff965a98d0c1c4381bc67b2041f04
dione24 <dioneniangaly@gmail.com>
2023-11-06 21:31:04 +0000

    DONE

--- Web/config/clientCount.php
+++ Web/config/clientCount.php
@@ -2,7 +2,7 @@
 require("db.php");
 if (isset($_GET['NumCompte'])) {
     $Nucompte = $_GET['NumCompte'];
-    $query = $baseDeDonnee->prepare("SELECT COUNT(*) as count FROM TbleOperations WHERE MONTH(Approve2_Time) = MONTH(CURRENT_DATE()) AND YEAR(Approve2_Time) = YEAR(CURRENT_DATE()) AND NumCompte = :NumCompte");
+    $query = $baseDeDonnee->prepare("SELECT COUNT(*) as count FROM TbleOperations WHERE MONTH(Approve2_Time) = MONTH(CURRENT_DATE()) AND YEAR(Approve2_Time) = YEAR(CURRENT_DATE()) AND NumCompte = :NumCompte AND TbleOperations.Approve2_Id IS NOT NULL AND TbleOperations.Reset_Id IS NULL");
     $query->bindValue(':NumCompte', $Nucompte, \PDO::PARAM_INT);
     $query->execute();
     $result = $query->fetch();

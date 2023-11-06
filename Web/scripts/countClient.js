af102222aba93c63c51120f7a465b86b4609d887
dione24 <dioneniangaly@gmail.com>
2023-11-06 21:27:42 +0000

    Fixed

--- Web/scripts/countClient.js
+++ Web/scripts/countClient.js
@@ -3,29 +3,41 @@ $(document).ready(function () {
         var accountNumber = $(this).data('account');
         var $this = $(this);
 
-        $.ajax({
-            url: '/config/clientCount.php',
-            type: 'GET',
-            data: { 'NumCompte': accountNumber },
-            success: function (data) {
-                // Assurez-vous que la réponse est au format JSON
-                try {
-                    var response = JSON.parse(data);
-                    console.log(response);
+        if ($this.data('original-content') === undefined) {
+            // Sauvegardez le contenu d'origine
+            $this.data('original-content', $this.text());
 
-                    if (response.count !== undefined) {
-                        var message = `(${response.count}) ${accountNumber}`;
-                        $this.text(message);
-                    } else {
-                        console.error("Champ 'count' manquant dans la réponse JSON.");
+            $.ajax({
+                url: '/config/clientCount.php',
+                type: 'GET',
+                data: { 'NumCompte': accountNumber },
+                success: function (data) {
+                    // Vérifiez la réponse en console pour le débogage
+                    console.log(data);
+
+                    // Assurez-vous que la réponse est au format JSON
+                    try {
+                        var response = JSON.parse(data);
+                        console.log(response);
+
+                        if (response.count !== undefined) {
+                            var message = `(${response.count}) ${accountNumber}`;
+                            $this.text(message);
+                        } else {
+                            console.error("Champ 'count' manquant dans la réponse JSON.");
+                        }
+                    } catch (e) {
+                        console.error("Erreur d'analyse JSON : " + e);
                     }
-                } catch (e) {
-                    console.error("Erreur d'analyse JSON : " + e);
+                },
+                error: function (xhr, status, error) {
+                    console.error("Erreur AJAX : " + error);
                 }
-            },
-            error: function (xhr, status, error) {
-                console.error("Erreur AJAX : " + error);
-            }
-        });
+            });
+        } else {
+            // Restaurez le contenu d'origine
+            $this.text($this.data('original-content'));
+            $this.removeData('original-content');
+        }
     });
 });

297049b3547e0fb54fb986271eb2e2036dbfacd1
dione24 <dioneniangaly@gmail.com>
2023-11-07 09:53:51 +0000

    Fixed

--- Web/config/clientCount.php
+++ Web/config/clientCount.php
@@ -1,6 +1,6 @@
 <?php
 require("db.php");
-if (isset($_GET['NumCompte'])) {
+if (isset($_GET['NumCompte']) && is_numeric($_GET['NumCompte'])) {
     $Nucompte = $_GET['NumCompte'];
     $query = $baseDeDonnee->prepare("SELECT COUNT(*) as count FROM TbleOperations WHERE MONTH(Approve2_Time) = MONTH(CURRENT_DATE()) AND YEAR(Approve2_Time) = YEAR(CURRENT_DATE()) AND NumCompte = :NumCompte AND TbleOperations.Approve2_Id IS NOT NULL AND TbleOperations.Reset_Id IS NULL");
     $query->bindValue(':NumCompte', $Nucompte, \PDO::PARAM_INT);
@@ -9,4 +9,6 @@ if (isset($_GET['NumCompte'])) {
 
     // Utilisez la fonction json_encode pour renvoyer une réponse JSON
     echo json_encode(['count' => $result['count']]);
+} else {
+    echo json_encode(['count' => null]);
 }

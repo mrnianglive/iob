176310d0c490280bbe282cc9d7dbdfa2cdad7e6b
dione24 <dioneniangaly@gmail.com>
2023-12-26 14:33:01 +0000

    Update user authentication logic

--- Applications/App/Modules/Bielletage/BielletageController.class.php
+++ Applications/App/Modules/Bielletage/BielletageController.class.php
@@ -313,7 +313,7 @@ class BielletageController extends \Library\BackController
 
         // Si toutes les vérifications sont passées, ajouter l'opération
         print_r($_POST);
-        $this->managers->getManagerOf("Bielletage")->Add();
+        // $this->managers->getManagerOf("Bielletage")->Add();
     }
 
     // Les fonctions auxiliaires telles que isRequiredApprovisionnement(), isValidMontantVersement(), et isValidSoldeCaisse()

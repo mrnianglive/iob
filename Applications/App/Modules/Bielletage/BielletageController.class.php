9023272a3ef1f90ae525463f457a554b523259c5
dione24 <dioneniangaly@gmail.com>
2024-01-04 11:39:40 +0000

    Refactor BielletageController class to remove unnecessary code and comments

--- Applications/App/Modules/Bielletage/BielletageController.class.php
+++ Applications/App/Modules/Bielletage/BielletageController.class.php
@@ -211,22 +211,8 @@ class BielletageController extends \Library\BackController
         }
 
         //Check if user has permission to view invoice Using CaisseID and UserID
-
-
-
-
         $Invoice  = $this->managers->getManagerOf("Bielletage")->GetInvoice($reference); //Recuperation de la liste
-
-
         $this->page->addVar("GetInvoice", $Invoice); // Creation de la variable, ajout d'une variable a la vue
-
-        // $userHasPermission = $this->managers->getManagerOf("Users")->VerifCaisse($Invoice['RefCaisse'], $_SESSION['RefUsers']);
-        // if (!$userHasPermission) {
-        //     $_SESSION['message']['type'] = 'warning';
-        //     $_SESSION['message']['text'] = 'Vous n\'avez pas la permission de voir cette facture';
-        //     $_SESSION['message']['number'] = 2;
-        //     $this->app()->httpResponse()->redirect('/'); // Redirection avec message
-        // }
 
         $getResetStatus = $this->managers->getManagerOf("Bielletage")->getResetStatus($reference);
         $this->page->addVar("getResetStatus", $getResetStatus); // Creation de la variable, ajout d'une variable a la vue

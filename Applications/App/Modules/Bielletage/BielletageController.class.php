6caec121fd56376e86d88251bd79ffe7318a4695
dione24 <dioneniangaly@gmail.com>
2024-01-04 11:38:48 +0000

    Commented out permission check for viewing invoice

--- Applications/App/Modules/Bielletage/BielletageController.class.php
+++ Applications/App/Modules/Bielletage/BielletageController.class.php
@@ -220,13 +220,13 @@ class BielletageController extends \Library\BackController
 
         $this->page->addVar("GetInvoice", $Invoice); // Creation de la variable, ajout d'une variable a la vue
 
-        $userHasPermission = $this->managers->getManagerOf("Users")->VerifCaisse($Invoice['RefCaisse'], $_SESSION['RefUsers']);
-        if (!$userHasPermission) {
-            $_SESSION['message']['type'] = 'warning';
-            $_SESSION['message']['text'] = 'Vous n\'avez pas la permission de voir cette facture';
-            $_SESSION['message']['number'] = 2;
-            $this->app()->httpResponse()->redirect('/'); // Redirection avec message
-        }
+        // $userHasPermission = $this->managers->getManagerOf("Users")->VerifCaisse($Invoice['RefCaisse'], $_SESSION['RefUsers']);
+        // if (!$userHasPermission) {
+        //     $_SESSION['message']['type'] = 'warning';
+        //     $_SESSION['message']['text'] = 'Vous n\'avez pas la permission de voir cette facture';
+        //     $_SESSION['message']['number'] = 2;
+        //     $this->app()->httpResponse()->redirect('/'); // Redirection avec message
+        // }
 
         $getResetStatus = $this->managers->getManagerOf("Bielletage")->getResetStatus($reference);
         $this->page->addVar("getResetStatus", $getResetStatus); // Creation de la variable, ajout d'une variable a la vue
